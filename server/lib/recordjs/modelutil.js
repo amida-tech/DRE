9086298b1261d91bcff276f691e07205fd6ce157
au2 <afsin@amida-tech.com>
2014-05-21 19:29:31 -0400

    tes-index and test-all-sections now pass

--- server/lib/recordjs/modelutil.js
+++ server/lib/recordjs/modelutil.js
@@ -15,6 +15,7 @@ limitations under the License.
 ======================================================================*/
 
 var jsutil = require('./jsutil');
+var _ = require('underscore');
 
 var mongooseCleanDocument = exports.mongooseCleanDocument = function(doc) {
     var id = doc._id;
@@ -47,26 +48,40 @@ exports.mongooseCleanFullRecord = function(record) {
 };
 
 var mongooseToBBModelDocument = exports.mongooseCleanDocument = function(doc) {
+    var result = _.clone(doc);
     ['_id', 'patKey', 'metadata'].forEach(function(prop) {
-        delete doc[prop];
+        delete result[prop];
     });
+    return result;
 }
 
 var mongooseToBBModelSection = exports.mongooseToBBModelSection = function(section) {
     if (Array.isArray(section)) { // all but demographics
         var n = section.length;
-        for (var i=0; i<n; ++i) {
-            var entry = section[i];
-            mongooseToBBModelDocument(entry);
+        if (n > 0) {
+            var result = [];
+            for (var i=0; i<n; ++i) {
+                var entry = section[i];
+                var entryResult = mongooseToBBModelDocument(entry);
+                result[i] = entryResult;
+            }
+            return result;
+        } else {
+            return section;
         }
     } else {
-        mongooseToBBModelDocument(section);
+        return mongooseToBBModelDocument(section);
     }
 }
 
 exports.mongooseToBBModelFullRecord = function(record) {
+    var result = {};
     Object.keys(record).forEach(function(sectionKey) {
         var section = record[sectionKey]
-        mongooseToBBModelSection(section);
+        if (section) {
+            section = mongooseToBBModelSection(section);
+        }
+        result[sectionKey] = section;
     });
+    return result;
 }
