9233aa10c734c96806dc99016c30a4fc52c74122
Matthew McCall <matt@mattmccall.com>
2014-05-29 14:33:45 -0700

    Added patient name to navbar.

--- client/src/common/services/getNotifications.js
+++ client/src/common/services/getNotifications.js
@@ -1,7 +1,10 @@
 angular.module('services.getNotifications', [])
 
-.service('getNotifications', ['$http',
-    function($http) {
+.service('getNotifications', ['$http', 'recordFunctions',
+    function($http, recordFunctions) {
+
+        var returnData = {};
+        returnData.data = {};
 
         this.getUpdate = function(callback) {
             $http({
@@ -13,18 +16,32 @@ angular.module('services.getNotifications', [])
                 var total_merges = data.notifications.new_merges + data.notifications.duplicate_merges;
                 data.notifications.total_merges = total_merges;
 
+                returnData.data = data;
+
                 if (data.notifications.total_merges > 0) {
                     data.notifications.displayNotifications = true;
                 } else {
                     data.notifications.displayNotifications = false;
                 }
-                //console.log(data.notifications);
-                callback(null, data.notifications);
+
+                //Added get demographics for name.
+                $http({
+                    method: 'GET',
+                    url: '/api/v1/record/demographics'
+                }).success(function(data, status, headers, config) {
+                    if (data.demographics.length > 0) {
+                        returnData.data.notifications.displayName = recordFunctions.formatName(data.demographics[0].name).displayName;    
+                    } else {
+                        returnData.data.notifications.displayName = ' ';
+                    }
+                    callback(null, returnData.data.notifications);
+                }).error(function(data, status, headers, config) {
+                    console.log(data);
+                });                
             }).
             error(function(data, status, headers, config) {
                 console.log(data);
             });
-
         };
 
     }
