61b8966bf1de2fbe1163608b2d4080d2af26ea9c
au2 <afsin@amida-tech.com>
2014-06-01 09:17:29 -0400

    use async, map, reduce instead of custom for loops

--- server/lib/recordjs/allsections.js
+++ server/lib/recordjs/allsections.js
@@ -1,61 +1,32 @@
 var section = require('./section');
 var record = require('./index');
+var async = require('async');
 
-exports.getAllSections = function(dbinfo, patientKey, callback) {
+exports.getAllSections = function(dbinfo, ptKey, callback) {
     var types = Object.keys(record.typeToSection);
-    var sectionCount = types.length;
-    var result = {};
-    var inError = false;
-    var count = 0;
-    types.forEach(function(type) {
-        section.getSection(dbinfo, type, patientKey, function(err, sectionResult) {
-            if (err) {
-                inError = true;
-                callback(err);
-            } else {
-                if (! inError) {
-                    var sectionName = record.typeToSection[type];
-                    result[sectionName] = sectionResult; 
-                    ++count;
-                    if (count === sectionCount) {
-                        callback(null, result);
-                    }
-                }
-            }
-        });
+    var f = function(type, cb) {
+        section.getSection(dbinfo, type, ptKey, cb);
+    };
+    async.map(types, f, function(err, sections) {
+        if (err) {
+            callback(err);
+        } else {
+            var accumulator = function(r, type, index) {
+                var name = record.typeToSection[type];
+                r[name] = sections[index];
+                return r;
+            };
+            var result = types.reduce(accumulator, {});
+            callback(null, result);
+        }
     });
 };
 
-exports.saveAllSectionsAsNew = function(dbinfo, patientKey, patientRecord, fileId, callback) {
+exports.saveAllSectionsAsNew = function(dbinfo, ptKey, master, fileId, callback) {
     var types = Object.keys(record.typeToSection);
-    var sectionCount = types.length;
-    var result = {};
-    var inError = false;
-    var count = 0;
-    
-    var onSectionHandled = function() {
-        ++count;
-        if (count === sectionCount) {
-            callback(null);
-        }
+    var f = function(type, cb) {
+        var name = record.typeToSection[type];
+        section.saveNewEntries(dbinfo, type, ptKey, master[name], fileId, cb);
     };
-    
-    types.forEach(function(type) {
-        var sectionName = record.typeToSection[type];
-        var sectionRecord = patientRecord[sectionName];
-        if (! sectionRecord) {
-            onSectionHandled();
-        } else {
-            section.saveNewEntries(dbinfo, type, patientKey, sectionRecord, fileId, function(err) {
-                if (err) {
-                    inError = true;
-                    callback(err);
-                } else {
-                    if (! inError) {
-                        onSectionHandled();
-                    }
-                }
-            });
-        }
-    });
+    async.map(types, f, callback);
 };
