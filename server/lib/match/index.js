a73df576306bedaa308a51a73f519c233180fcbf
au2 <afsin@amida-tech.com>
2014-06-26 13:55:40 -0400

    entry_type fixes

--- server/lib/match/index.js
+++ server/lib/match/index.js
@@ -77,43 +77,43 @@ function formatName(inputName) {
 function formatMerges(inputMerge) {
     for (var iMerge in inputMerge) {
         //Give Immunizations a name
-        if (inputMerge[iMerge].entry_type === 'immunization') {
-            if (inputMerge[iMerge].entry_id.product.name) {
-                inputMerge[iMerge].entry_id.name = inputMerge[iMerge].entry_id.product.name;
+        if (inputMerge[iMerge].entry_type === 'immunizations') {
+            if (inputMerge[iMerge].entry.product.name) {
+                inputMerge[iMerge].entry.name = inputMerge[iMerge].entry.product.name;
             }
-            if (inputMerge[iMerge].match_entry_id.product.name) {
-                inputMerge[iMerge].match_entry_id.name = inputMerge[iMerge].match_entry_id.product.name;
+            if (inputMerge[iMerge].match_entry.product.name) {
+                inputMerge[iMerge].match_entry.name = inputMerge[iMerge].match_entry.product.name;
             }
 
         }
         //Give Medications a name
-        if (inputMerge[iMerge].entry_type === 'medication') {
-            if (inputMerge[iMerge].entry_id.product.name) {
-                inputMerge[iMerge].entry_id.name = inputMerge[iMerge].entry_id.product.name;
+        if (inputMerge[iMerge].entry_type === 'medications') {
+            if (inputMerge[iMerge].entry.product.name) {
+                inputMerge[iMerge].entry.name = inputMerge[iMerge].entry.product.name;
             }
-            if (inputMerge[iMerge].match_entry_id.product.name) {
-                inputMerge[iMerge].match_entry_id.name = inputMerge[iMerge].match_entry_id.product.name;
+            if (inputMerge[iMerge].match_entry.product.name) {
+                inputMerge[iMerge].match_entry.name = inputMerge[iMerge].match_entry.product.name;
             }
         }
         //Give Socials a name
-        if (inputMerge[iMerge].entry_type === 'social') {
-            if (inputMerge[iMerge].entry_id.value) {
-                inputMerge[iMerge].entry_id.name = inputMerge[iMerge].entry_id.value;
+        if (inputMerge[iMerge].entry_type === 'social_history') {
+            if (inputMerge[iMerge].entry.value) {
+                inputMerge[iMerge].entry.name = inputMerge[iMerge].entry.value;
             }
-            if (inputMerge[iMerge].match_entry_id.value) {
-                inputMerge[iMerge].match_entry_id.name = inputMerge[iMerge].match_entry_id.value;
+            if (inputMerge[iMerge].match_entry.value) {
+                inputMerge[iMerge].match_entry.name = inputMerge[iMerge].match_entry.value;
             }
         }
         //Give Demographics a name
-        if (inputMerge[iMerge].entry_type === 'demographic') {
+        if (inputMerge[iMerge].entry_type === 'demographics') {
             var tmpName;
-            if (inputMerge[iMerge].entry_id.name) {
-                tmpName = formatName(inputMerge[iMerge].entry_id.name).displayName;
-                inputMerge[iMerge].entry_id.name = tmpName;
+            if (inputMerge[iMerge].entry.name) {
+                tmpName = formatName(inputMerge[iMerge].entry.name).displayName;
+                inputMerge[iMerge].entry.name = tmpName;
             }
-            if (inputMerge[iMerge].match_entry_id.name) {
-                tmpName = formatName(inputMerge[iMerge].match_entry_id.name).displayName;
-                inputMerge[iMerge].match_entry_id.name = tmpName;
+            if (inputMerge[iMerge].match_entry.name) {
+                tmpName = formatName(inputMerge[iMerge].match_entry.name).displayName;
+                inputMerge[iMerge].match_entry.name = tmpName;
             }
         }
     }
