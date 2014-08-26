e29ec106daab36107ecabb5f025b17ee42a2ba37
Matthew McCall <matt@mattmccall.com>
2014-08-26 10:40:59 -0700

    Vitals matching passes.

--- server/test/e2e/components/vitals.js
+++ server/test/e2e/components/vitals.js
@@ -264,7 +264,7 @@ describe('Vitals API - Test New/Dupe Mix:', function() {
 });
 
 //Modified severity on 2nd and 3rd allergy.  Changed Nausea to Hives on first allergy.
-xdescribe('Vitals API - Test Partial Matches:', function() {
+describe('Vitals API - Test Partial Matches:', function() {
 
 	before(function(done) {
 		loadTestRecord('bluebutton-04-diff-source-partial-matches.xml', function(err) {
@@ -283,7 +283,7 @@ xdescribe('Vitals API - Test Partial Matches:', function() {
 				if (err) {
 					return done(err);
 				}
-				//console.log(JSON.stringify(res.body.allergies, null, 10));
+				//console.log(JSON.stringify(res.body, null, 10));
 				expect(res.body.vitals.length).to.equal(11);
 				done();
 			});
@@ -334,7 +334,7 @@ xdescribe('Vitals API - Test Partial Matches:', function() {
 
 });
 
-xdescribe('Vitals API - Test Added Matches', function() {
+describe('Vitals API - Test Added Matches', function() {
 
 	var update_id = '';
 	var match_id = '';
@@ -431,7 +431,7 @@ xdescribe('Vitals API - Test Added Matches', function() {
 
 
 
-xdescribe('Vitals API - Test Ignored Matches', function() {
+describe('Vitals API - Test Ignored Matches', function() {
 
 	var update_id = '';
 	var match_id = '';
@@ -524,7 +524,7 @@ xdescribe('Vitals API - Test Ignored Matches', function() {
 });
 
 
-xdescribe('Vitals API - Test Merged Matches', function() {
+describe('Vitals API - Test Merged Matches', function() {
 
 	var match_id = '';
 
