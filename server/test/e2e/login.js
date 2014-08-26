f90c6c8e4c83a2226d0df99c81c7f008ceea7851
Ekavali Mishra <ekavali@amida-tech.com>
2014-08-26 15:04:22 -0400

    Login tests
    Register and login test

---
+++ server/test/e2e/login.js
@@ -0,0 +1,88 @@
+var assert = require("assert");
+var request = require('supertest');
+var api = request.agent('http://localhost:3000');
+var expect = require('chai').expect;
+var common = require('./common.js');
+
+describe('Array', function() {
+    describe('#indexOf()', function() {
+        xit('should return -1 when the value is not present', function() {
+            assert.equal(-1, [1, 2, 3].indexOf(5));
+            assert.equal(-1, [1, 2, 3].indexOf(0));
+        })
+    })
+})
+
+
+
+describe('authentication', function(done) {
+    it('should be unauthenticated', function(done) {
+        api
+            .get('/api/v1/account')
+            .expect(200)
+            .end(function(err, res) {
+                if (err) throw err;
+                expect(res.body.authenticated).to.equal(false);
+                done();
+            });
+    })
+
+
+
+    it('should register', function(done) {
+        api
+            .post('/api/v1/register')
+            .send({'username': 'test2', 'password':'test2'})
+            .end(function(err, res) {
+                if (err) throw err;
+                done();
+            });
+    })
+
+    it('should still be unauthenticated', function(done) {
+        api
+            .get('/api/v1/account')
+            .expect(200)
+            .end(function(err, res) {
+                if (err) throw err;
+                expect(res.body.authenticated).to.equal(false);
+                done();
+            });
+    })    
+
+    it('should login', function(done) {
+        common.login(api, 'test2', 'test2', done);
+    })
+
+    it('should be authenticated', function(done) {
+        api
+            .get('/api/v1/account')
+            .expect(200)
+            .end(function(err, res) {
+                if (err) throw err;
+                expect(res.body.authenticated).to.equal(true);
+                done();
+            });
+    })
+
+    it('should logout', function(done) {
+        api
+            .post('/api/v1/logout')
+            .expect(200)
+            .end(function(err, res) {
+                if (err) throw err;
+                done();
+            });
+    })
+
+    it('should be unauthenticated again', function(done) {
+        api
+            .get('/api/v1/account')
+            .expect(200)
+            .end(function(err, res) {
+                if (err) throw err;
+                expect(res.body.authenticated).to.equal(false);
+                done();
+            });
+    })                   
+})
