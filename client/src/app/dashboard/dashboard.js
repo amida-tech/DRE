b875fdd425ca14d19b21074807c46c0af221f3ae
Ekavali Mishra <ekavali@amida-tech.com>
2014-09-29 13:10:34 -0400

    Consolidate dashboard and my files

--- client/src/app/dashboard/dashboard.js
+++ client/src/app/dashboard/dashboard.js
@@ -25,9 +25,77 @@ angular.module('dre.dashboard', [])
   }
 ])
 
-.controller('dashboardCtrl', ['$scope', '$http', '$location',
-  function($scope, $http, $location) {
+.controller('dashboardCtrl', ['$rootScope', '$scope', '$route', '$http', '$location', 'fileUpload', 'getNotifications',
+  function($rootScope, $scope, $route, $http, $location, fileUpload, getNotifications) {
 
+    $scope.predicate = "-file_upload_date";
+
+    $scope.nameSort = function () {
+      if ($scope.predicate.substring(0,1) === "-") {
+        if ($scope.predicate.substring(1) === "file_name") {
+          $scope.predicate = "file_name";
+        } else {
+          $scope.predicate = "-file_name";
+        }
+      } else {
+        $scope.predicate = "-file_name";
+      }
+    };
+
+    $scope.dateSort = function () {
+      if ($scope.predicate.substring(0,1) === "-") {
+        if ($scope.predicate.substring(1) === "file_upload_date") {
+          $scope.predicate = "file_upload_date";
+        } else {
+          $scope.predicate = "-file_upload_date";
+        }
+      } else {
+        $scope.predicate = "-file_upload_date";
+      }
+    };
+
+    $scope.file_array = [];
+    $scope.displayRecords = false;
+
+    $scope.refreshRecords = function() {
+      $http({
+        method: 'GET',
+        url: '/api/v1/storage'
+      }).
+      success(function(data, status, headers, config) {
+        $scope.file_array = data.storage;
+        getNotifications.getUpdate(function(err, notifications) {
+          $rootScope.notifications = notifications;
+        });
+        if ($scope.file_array.length > 0) {
+          $scope.displayRecords = true;
+        } else {
+          $scope.displayRecords = false;
+        }
+      }).
+      error(function(data, status, headers, config) {
+      });
+    };
+
+    $scope.refreshRecords();
+
+    //File upload.
+    $scope.uploadRecord = function() {
+
+      var uploadFile = $scope.myFile;
+      var uploadUrl = "/api/v1/storage";
+      fileUpload.uploadFileToUrl(uploadFile, uploadUrl, function(err, res) {
+        if (err) {
+          console.log(err);
+        }
+
+        angular.element("div").removeClass("modal-backdrop fade in");
+        $scope.myFile = null;
+        $scope.refreshRecords();
+        //$route.reload();
+      });
+
+    };
 
   }
 ]);
