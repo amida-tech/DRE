e928b3df20169e863e34deadec9d650a6932f8db
Ekavali Mishra <ekavali@amida-tech.com>
2014-10-08 02:13:31 -0400

    Dashboard
    Add ‘completed updates’, ‘upload’ and ‘download’ features

--- client/src/app/dashboard/dashboard.js
+++ client/src/app/dashboard/dashboard.js
@@ -17,85 +17,158 @@ limitations under the License.
 angular.module('dre.dashboard', [])
 
 .config(['$routeProvider',
-  function($routeProvider) {
-    $routeProvider.when('/dashboard', {
-      templateUrl: 'templates/dashboard/dashboard.tpl.html',
-      controller: 'dashboardCtrl'
-    });
-  }
+    function($routeProvider) {
+        $routeProvider.when('/dashboard', {
+            templateUrl: 'templates/dashboard/dashboard.tpl.html',
+            controller: 'dashboardCtrl'
+        });
+    }
 ])
 
-.controller('dashboardCtrl', ['$rootScope', '$scope', '$route', '$http', '$location', 'fileUpload', 'getNotifications',
-  function($rootScope, $scope, $route, $http, $location, fileUpload, getNotifications) {
+.controller('dashboardCtrl', ['$rootScope', '$scope', '$route', '$http', '$location', 'fileUpload', 'getNotifications', 'recordFunctions',
+    function($rootScope, $scope, $route, $http, $location, fileUpload, getNotifications, recordFunctions) {
 
-    $scope.predicate = "-file_upload_date";
-
-    $scope.nameSort = function () {
-      if ($scope.predicate.substring(0,1) === "-") {
-        if ($scope.predicate.substring(1) === "file_name") {
-          $scope.predicate = "file_name";
-        } else {
-          $scope.predicate = "-file_name";
-        }
-      } else {
-        $scope.predicate = "-file_name";
-      }
-    };
-
-    $scope.dateSort = function () {
-      if ($scope.predicate.substring(0,1) === "-") {
-        if ($scope.predicate.substring(1) === "file_upload_date") {
-          $scope.predicate = "file_upload_date";
-        } else {
-          $scope.predicate = "-file_upload_date";
-        }
-      } else {
         $scope.predicate = "-file_upload_date";
-      }
-    };
+        $scope.new_merges = [];
+        $scope.duplicate_merges = [];
+        $scope.predicate = "-merged";
+        $scope.displayMerges = false;
 
-    $scope.file_array = [];
-    $scope.displayRecords = false;
+        $scope.updateSort = function() {
+            if ($scope.predicate.substring(0, 1) === "-") {
+                if ($scope.predicate.substring(1) === "entry_type") {
+                    $scope.predicate = "entry_type";
+                } else {
+                    $scope.predicate = "-entry_type";
+                }
+            } else {
+                $scope.predicate = "-entry_type";
+            }
+        };
 
-    $scope.refreshRecords = function() {
-      $http({
-        method: 'GET',
-        url: '/api/v1/storage'
-      }).
-      success(function(data, status, headers, config) {
-        $scope.file_array = data.storage;
-        getNotifications.getUpdate(function(err, notifications) {
-          $rootScope.notifications = notifications;
-        });
-        if ($scope.file_array.length > 0) {
-          $scope.displayRecords = true;
-        } else {
-          $scope.displayRecords = false;
-        }
-      }).
-      error(function(data, status, headers, config) {
-      });
-    };
+        $scope.elementSort = function() {
+            if ($scope.predicate.substring(0, 1) === "-") {
+                if ($scope.predicate.substring(1) === "entry.name") {
+                    $scope.predicate = "entry.name";
+                } else {
+                    $scope.predicate = "-entry.name";
+                }
+            } else {
+                $scope.predicate = "-entry.name";
+            }
+        };
 
-    $scope.refreshRecords();
 
-    //File upload.
-    $scope.uploadRecord = function() {
+        $scope.nameSort = function() {
+            if ($scope.predicate.substring(0, 1) === "-") {
+                if ($scope.predicate.substring(1) === "file_name") {
+                    $scope.predicate = "file_name";
+                } else {
+                    $scope.predicate = "-file_name";
+                }
+            } else {
+                $scope.predicate = "-file_name";
+            }
+        };
 
-      var uploadFile = $scope.myFile;
-      var uploadUrl = "/api/v1/storage";
-      fileUpload.uploadFileToUrl(uploadFile, uploadUrl, function(err, res) {
-        if (err) {
-          console.log(err);
-        }
+        $scope.dateSort = function() {
+            if ($scope.predicate.substring(0, 1) === "-") {
+                if ($scope.predicate.substring(1) === "merged") {
+                    $scope.predicate = "merged";
+                } else {
+                    $scope.predicate = "-merged";
+                }
+            } else {
+                $scope.predicate = "-merged";
+            }
+        };
 
-        angular.element("div").removeClass("modal-backdrop fade in");
-        $scope.myFile = null;
+        $scope.file_array = [];
+        $scope.displayRecords = false;
+
+        $scope.refreshRecords = function() {
+            $http({
+                method: 'GET',
+                url: '/api/v1/storage'
+            }).
+            success(function(data, status, headers, config) {
+                $scope.file_array = data.storage;
+                getNotifications.getUpdate(function(err, notifications) {
+                    $rootScope.notifications = notifications;
+                });
+                if ($scope.file_array.length > 0) {
+                    $scope.displayRecords = true;
+                } else {
+                    $scope.displayRecords = false;
+                }
+            }).
+            error(function(data, status, headers, config) {});
+        };
+
         $scope.refreshRecords();
-        //$route.reload();
-      });
 
-    };
+        //File upload.
+        $scope.uploadRecord = function() {
 
-  }
+            var uploadFile = $scope.myFile;
+            var uploadUrl = "/api/v1/storage";
+            fileUpload.uploadFileToUrl(uploadFile, uploadUrl, function(err, res) {
+                if (err) {
+                    console.log(err);
+                }
+
+                angular.element("div").removeClass("modal-backdrop fade in");
+                $scope.myFile = null;
+                $scope.refreshRecords();
+                //$route.reload();
+            });
+
+        };
+
+        function formatMerges(inputMerge) {
+            var trimLength = 35;
+            for (var iMerge in inputMerge) {
+                if (inputMerge[iMerge].entry_type !== 'demographics') {
+                    recordFunctions.extractName(inputMerge[iMerge].entry, inputMerge[iMerge].entry_type);
+                }
+
+                if (inputMerge[iMerge].entry_type === 'demographics') {
+                    var tmpName = recordFunctions.formatName(inputMerge[iMerge].entry.name);
+                    inputMerge[iMerge].entry.name = tmpName.displayName;
+                }
+            }
+        }
+
+
+        $http({
+            method: 'GET',
+            url: '/api/v1/merges'
+        }).
+        success(function(data, status, headers, config) {
+            if (data.merges.length > 0) {
+                console.log('data merges on success');
+                console.log(data.merges);
+                $scope.displayMerges = true;
+            } else {
+                $scope.displayMerges = false;
+            }
+
+
+            for (var i = 0; i < data.merges.length; i++) {
+                data.merges[i].section_singular = recordFunctions.singularizeSection(data.merges[i].entry_type);
+                if (data.merges[i].merge_reason === "duplicate") {
+                    $scope.duplicate_merges.push(data.merges[i]);
+                } else {
+                    $scope.new_merges.push(data.merges[i]);
+                }
+            }
+
+            formatMerges($scope.new_merges);
+
+        }).
+        error(function(data, status, headers, config) {
+            //console.log('error');
+        });
+
+    }
 ]);
