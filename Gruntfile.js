f96c9253286be1e4ca5fc920ee9e3fbca0f56246
Matthew McCall <matt@mattmccall.com>
2014-11-03 12:42:56 -0800

    advertising space on main page.

--- Gruntfile.js
+++ Gruntfile.js
@@ -353,6 +353,11 @@ module.exports = function (grunt) {
           cwd: '.',
           src: 'bower_components/bootstrap-sass-official/assets/fonts/bootstrap/*',
           dest: '<%= yeoman.dist %>'
+        }, {
+          expand: true,
+          cwd: '.',
+          src: 'bower_components/font-awesome/fonts/*',
+          dest: '<%= yeoman.dist %>'
         }]
       },
       styles: {
