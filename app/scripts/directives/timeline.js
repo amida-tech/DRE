1ad9f0ccca8336efc243206cd174d24997fd3295
Matthew McCall <matt@mattmccall.com>
2014-11-07 11:27:51 -0800

    month offset in date handling.

--- app/scripts/directives/timeline.js
+++ app/scripts/directives/timeline.js
@@ -12,71 +12,161 @@ angular.module('phrPrototypeApp')
             restrict: 'EA',
             template: "<svg style='width:100%;'></svg>",
             link: function postLink(scope, element, attrs) {
-                
-            	// Find SVG on the page.
+
+            	var plotHeight = 50;
+            	var boundaryOffset = 15;
+            	var boundaryWidth = 5;
+
+                var plotCircles = [];
+                var plotDomain = [];
+                var timeScale;
                 var d3 = $window.d3;
                 var rawSvg = element.find("svg")[0];
-                var svg = d3.select(rawSvg);
+                var svg = d3.select(rawSvg).attr("height", plotHeight);
+                var format = d3.time.format("%m/%d/%Y");
 
-            	var width = parseInt(svg.style('width'), 10);
+                function gatherData() {
+                    var dataToPlot = scope[attrs.chartData];
+                    var dataType = attrs.chartType;
+                    var tmpDomain = [];
 
-            	svg.attr("height", 50);
+                    if (dataType === 'account') {
 
-            	var plotCircles = [];
-            	var plotDomain = [];
-            	var timeScale;
+                        for (var i in dataToPlot.recordHistory) {
+                            var plotDate = format.parse(dataToPlot.recordHistory[i].date);
+                            plotCircles.push({
+                                "date": plotDate
+                            });
+                            tmpDomain.push(plotDate);
+                        }
 
-            	var format = d3.time.format("%m/%d/%Y");
+                        var minDate = d3.min(tmpDomain);
+                        var maxDate = d3.max(tmpDomain);
+                        var plotFloor = d3.time.month.floor(d3.time.month.offset(minDate, -1));
+                        var plotCeiling = d3.time.month.floor(d3.time.month.offset(maxDate, 1));
+                        plotDomain = [plotFloor, plotCeiling];
+                    }
+                }
 
-            	function gatherData () {
-            		var dataToPlot = scope[attrs.chartData];
-            		var dataType = attrs.chartType;
-            		if (dataType === 'account') {
-            			
-            			for (var i in dataToPlot.recordHistory) {
-            				var plotDate = format.parse(dataToPlot.recordHistory[i].date);
-            				plotCircles.push({"date": plotDate});
-            				plotDomain.push(plotDate);
-            			}
-            		}
-            	}
+                function renderPlot() {
 
-            	function buildScale () {
-	            	timeScale = d3.time.scale()
-	    				.range([0, width])
-	    				.domain(plotDomain);
-            	}
+                	var width;
 
-            	function structureData () {
-            		for (var i in plotCircles) {
-            			plotCircles[i].x_axis = timeScale(plotCircles[i].date);
-            			plotCircles[i].y_axis = 30;
-            			plotCircles[i].radius = 10;
-            			plotCircles[i].color = "green";
-            		}
-            	}
+                    function getSVGWidth() {
+                        width = parseInt(svg.style('width'), 10);
+                    }
 
-            	gatherData();
-            	buildScale();
-            	structureData();
+                    function buildScale() {
 
-            	var circles = svg.selectAll("circle").data(plotCircles).enter().append("circle");
+                        timeScale = d3.time.scale()
+                            .range([(boundaryOffset + (boundaryWidth / 2)),width - (boundaryOffset - (boundaryWidth / 2 ))])
+                            .domain(plotDomain);
+                    }
 
-            	var circleAttributes = circles
-                     .attr("cx", function (d) { return d.x_axis; }
-                       )
-                      .attr("cy", function (d) { return d.y_axis; })
-                      .attr("r", function (d) { return d.radius; })
-                      .style("fill", function(d) { return d.color; });
+                    function structureData() {
+                        for (var i in plotCircles) {
+                            plotCircles[i].x_axis = timeScale(plotCircles[i].date);
+                            plotCircles[i].y_axis = plotHeight / 2;
+                            plotCircles[i].radius = 10;
+                            plotCircles[i].color = "green";
+                        }
+                    }
+
+                    function plotData() {
+                        svg.selectAll('*').remove();
+
+var boundaryData = [{
+                        "x": boundaryOffset,
+                        "y": 0,
+                        "width": boundaryWidth,
+                        "height": plotHeight,
+                        "color": "orange"
+                    }, {
+                        "x": width - boundaryOffset,
+                        "y": 0,
+                        "width": boundaryWidth,
+                        "height": plotHeight,
+                        "color": "orange"
+                    }];
+
+                    var boundaries = svg.selectAll("rect").data(boundaryData).enter().append("rect");
+
+                    var boundaryAttributes = boundaries
+                            .attr("x", function (d) {
+                                return d.x;
+                            })
+                            .attr("y", function (d) {
+                                return d.y;
+                            })
+                            .attr("width", function (d) {
+                                return d.width;
+                            })
+                            .attr("height", function (d) {
+                                return d.height;
+                            })
+                            .style("fill", function (d) {
+                                return d.color;
+                            });
 
 
-            	//console.log(plotDomain);
-            	//console.log(plotCircles);
-
-            	//console.log(timeScale(plotDomain[1]));
 
 
-            	/*
+                        var circles = svg.selectAll("circle").data(plotCircles).enter().append("circle");
+                        var circleAttributes = circles
+                            .attr("cx", function (d) {
+                                return d.x_axis;
+                            })
+                            .attr("cy", function (d) {
+                                return d.y_axis;
+                            })
+                            .attr("r", function (d) {
+                                return d.radius;
+                            })
+                            .style("fill", function (d) {
+                                return d.color;
+                            });
+
+
+
+                    
+
+
+                    }
+
+                    getSVGWidth();
+                    buildScale();
+                    structureData();
+                    plotData();
+
+                }
+
+                //gatherData only on first run.
+                gatherData();
+                renderPlot();
+
+                //Re-evaluate scope on resize.
+                $window.onresize = function () {
+                    scope.$apply();
+                    renderPlot();
+                };
+
+                /*function resize() {
+    // update width
+    width = parseInt(d3.select('#chart').style('width'), 10);
+    width = width - margin.left - margin.right;
+
+    // reset x range
+    x.range([0, width]);
+
+    // do the actual resize...
+}*/
+
+                //console.log(plotDomain);
+                //console.log(plotCircles);
+
+                //console.log(timeScale(plotDomain[1]));
+
+                /*
             	// Sample Circles.  Real circles will css class applied, x-axis only adjustment.
                 var jsonCircles = [
                 	{ "x_axis": 30, "y_axis": 30, "radius": 20, "color" : "green" },
