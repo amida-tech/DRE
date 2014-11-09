b23b4502e0a463fb7d0864848d21e4b5e6c9a102
Matthew McCall <matt@mattmccall.com>
2014-11-09 10:47:01 -0800

    add email as login.

--- app/scripts/services/authentication.js
+++ app/scripts/services/authentication.js
@@ -15,7 +15,7 @@ angular.module('phrPrototypeApp')
         this.login = function (username, password, callback) {
             if (username && password) {
                 //Stubbed login.
-                if (username === 'test' && password === 'test') {
+                if ((username === 'test' && password === 'test') || (username === 'test@amida-demo.com' && password === 'test')) {
                     callback(null);
                 } else {
                     callback('Invalid Login or Password');
