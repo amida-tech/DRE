0134b04a62e6931119fa869090a1ec1db23a9f90
Matthew McCall <matt@mattmccall.com>
2014-11-10 10:46:50 -0800

    Basic profile page.

--- app/scripts/controllers/profile.js
+++ app/scripts/controllers/profile.js
@@ -8,10 +8,32 @@
  * Controller of the phrPrototypeApp
  */
 angular.module('phrPrototypeApp')
-  .controller('ProfileCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('ProfileCtrl', function ($scope, $location, $anchorScroll, account) {
+
+  	$scope.profile = {};
+
+    $scope.navClick = function (element) {
+        var old = $location.hash();
+        $location.hash(element);
+        $anchorScroll();
+        //reset to old to keep any additional routing logic from kicking in
+        $location.hash(old);
+    };
+
+  	account.account(function(err, accountInfo) {
+
+  		
+  		$scope.profile = accountInfo;
+
+  		//Shims for HL7 weirdness.
+  		var tmpDOB = moment(accountInfo.dob[0].date).format('YYYY-MM-DD');
+
+  		console.log(tmpDOB);
+
+  		$scope.profile.dob = tmpDOB;
+  		$scope.profile.primaryEmail = accountInfo.email[0].email;
+  	});
+
+  	//console.log($scope.profile);
+
   });
