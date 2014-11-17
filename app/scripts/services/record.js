132990ce6a1bb287922ecddba6d3bc0cde2e3372
Matthew McCall <matt@mattmccall.com>
2014-11-17 16:44:29 -0500

    Basic records dashboard.

--- app/scripts/services/record.js
+++ app/scripts/services/record.js
@@ -8,14 +8,14 @@
  * Service in the phrPrototypeApp.
  */
 angular.module('phrPrototypeApp')
-  .service('record', function record(allergies, vitals) {
+  .service('record', function record(allergies, conditions, encounters, immunizations, medications, procedures, results, social, vitals) {
    	
   	this.getRecord = function(callback) {
 
   		var masterRecord = {};
 
       var iter = 0;
-      var iterLength = 2;
+      var iterLength = 9;
 
       function tryCallback () {
 
@@ -26,19 +26,57 @@ angular.module('phrPrototypeApp')
       }
 
   		allergies.getRecord(function(err, allergies) {
-
   			masterRecord.allergies = allergies;
         iter++;
-        tryCallback();
-  			
+        tryCallback();	
   		});
 
-      vitals.getRecord(function(err, vitals) {
+      conditions.getRecord(function(err, conditions) {
+        masterRecord.conditions = conditions;
+        iter++;
+        tryCallback();  
+      });
 
+      encounters.getRecord(function(err, encounters) {
+        masterRecord.encounters = encounters;
+        iter++;
+        tryCallback();  
+      });
+
+      immunizations.getRecord(function(err, immunizations) {
+        masterRecord.immunizations = immunizations;
+        iter++;
+        tryCallback();  
+      });
+
+      medications.getRecord(function(err, medications) {
+        masterRecord.medications = medications;
+        iter++;
+        tryCallback();  
+      });
+
+      procedures.getRecord(function(err, procedures) {
+        masterRecord.procedures = procedures;
+        iter++;
+        tryCallback();  
+      });
+
+      results.getRecord(function(err, results) {
+        masterRecord.results = results;
+        iter++;
+        tryCallback();  
+      });    
+
+      social.getRecord(function(err, social) {
+        masterRecord.social = social;
+        iter++;
+        tryCallback();  
+      });    
+
+      vitals.getRecord(function(err, vitals) {
         masterRecord.vitals = vitals;
         iter++;
         tryCallback();
-
       });
 
   	};
