c8185a8cd50f10846cc24a68608ba72a9da269b4
Ekavali Mishra <ekavali@amida-tech.com>
2014-11-18 16:12:46 -0500

    first pass and charges

--- app/scripts/controllers/billing/claims.js
+++ app/scripts/controllers/billing/claims.js
@@ -8,10 +8,57 @@
  * Controller of the phrPrototypeApp
  */
 angular.module('phrPrototypeApp')
-  .controller('BillingClaimsCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+    .controller('BillingClaimsCtrl', function ($scope, claims, format) {
+
+
+        $scope.entryType = 'claims';
+        $scope.masterEntries = [];
+        $scope.entries = [];
+        $scope.updateDate = null;
+
+        function getUpdateDate() {
+            //Should grab from files/update history.  Stubbed for now.
+            $scope.updateDate = '12/1/2014';
+        }
+
+        function getRecords(callback) {
+            claims.getRecord(function(err, results) {
+                $scope.masterEntries = results;
+                callback();
+            });
+        }
+
+        function formatDates() {
+            //Add displayDate to all entries.
+            _.each($scope.masterEntries, function (entry) {
+                if (entry.start_date) {
+                    _.each(entry.start_date, function(dateEntry) {
+                        format.formatDate(dateEntry);
+                    });
+                    entry.start_date.displayDate = format.outputDate(entry.start_date);
+                    entry.start_date.plotDate = format.plotDate(entry.start_date);
+                }
+            });
+        }
+
+        function formatAddress() {
+            _.each($scope.masterEntries, function(entry) {
+                _.each(entry.locations, function(loc) {
+                    _.each(loc.address, function(addr) {
+                        format.formatAddress(addr);
+                    });
+                });
+            });
+        }
+
+        $scope.refresh = function() {
+            getRecords(function(err) {
+                getUpdateDate();
+                formatDates();
+                formatAddress();
+                $scope.entries = $scope.masterEntries;
+            });
+        }
+
+        $scope.refresh();
+    });
--- app/scripts/services/billing/claims.js
+++ app/scripts/services/billing/claims.js
@@ -8,6 +8,80 @@
  * Service in the phrPrototypeApp.
  */
 angular.module('phrPrototypeApp')
-  .service('billing/claims', function () {
-    // AngularJS will instantiate a singleton by calling "new" on this function
-  });
+    .service('claims', function claims() {
+
+        this.getRecord = function(callback) {
+
+                var tmpClaims = [{
+
+                    "payer": [
+                        "medicare"
+                    ],
+                    "number": "1234567890000",
+                    "start_date": {
+                        "date": "2012-10-18T00:00:00Z",
+                        "precision": "day"
+                    },
+                    "lines": [{
+                        "line": "1",
+                        "start_date": {
+                            "date": "2012-10-18T00:00:00Z",
+                            "precision": "day"
+                        },
+                        "end_date": {
+                            "date": "2012-10-18T00:00:00Z",
+                            "precision": "day"
+                        },
+                        "procedure": {
+                            "code": "98941",
+                            "description": "Chiropractic Manipulative Treatment (Cmt); Spinal, Three To Four Regions"
+                        },
+                        "quantity": 1,
+                        "modifier": [{
+                            "code": "98941",
+                            "description": "Chiropractic Manipulative Treatment (Cmt); Spinal, Three To Four Regions"
+                        }],
+                        "charges": {
+                            "price_billed": "$60.00",
+                            "insurance_paid": "$34.00"
+                        },
+                        "place": {
+                            "code": "11",
+                            "name": "Office"
+                        },
+                        "type": {
+                            "code": "1",
+                            "name": "Medical Care"
+                        },
+                        "rendering_provider": {
+                            "number": "0000001",
+                            "npi": "123456789"
+                        }
+                    }],
+                    "charges": {
+                        "price_billed": "$60.00",
+                        "provider_paid": "$27.20",
+                        "patient_responsibility": "$6.80"
+                    },
+                    "type": [
+                        "medicare PartB"
+                    ],
+                    "diagnosis": [{
+                        "code": "3534"
+                    }, {
+                        "code": "7393"
+                    }, {
+                        "code": "7392"
+                    }, {
+                        "code": "3533"
+                    }],
+                    "provider": {
+                        "name": "No Information Available"
+                    }
+                }];
+
+                callback(null, tmpClaims);
+
+            }
+            // AngularJS will instantiate a singleton by calling "new" on this function
+    });
