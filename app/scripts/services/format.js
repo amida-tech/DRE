d4b039e537b74ae09532a31faef60009f88587c9
Ekavali Mishra <ekavali@amida-tech.com>
2014-11-20 16:45:32 -0500

    Revert plotdate changes

--- app/scripts/services/format.js
+++ app/scripts/services/format.js
@@ -80,8 +80,6 @@ angular.module('phrPrototypeApp')
                 return date_time.low.date;
             } else if (date_time.center) {
                 return date_time.center.date;
-            } else if (date_time.date) {
-                return date_time.date;
             } else {
                 return null;
             }
