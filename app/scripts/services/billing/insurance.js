8aacc520559cec98d9dee3ad947eda8faaf3a6e3
Ekavali Mishra <ekavali@amida-tech.com>
2014-11-20 13:44:56 -0500

    finish insurance

--- app/scripts/services/billing/insurance.js
+++ app/scripts/services/billing/insurance.js
@@ -41,69 +41,6 @@ angular.module('phrPrototypeApp')
                     "date": "2011-12-31T00:00:00Z",
                     "precision": "day"
                 }]
-            }, {
-                "type": "medicare msp:End stage Renal Disease (ESRD)",
-                "policy_number": "1234567890",
-                "payer_name": "Insurer1",
-                "date": [{
-                    "date": "2011-01-01T00:00:00Z",
-                    "precision": "day"
-                }, {
-                    "date": "2011-09-30T00:00:00Z",
-                    "precision": "day"
-                }],
-                "addresses": [{
-                    "use": "primary",
-                    "streetLines": [
-                        "PO BOX 0000"
-                    ],
-                    "city": "Anytown",
-                    "state": "CO",
-                    "zip": "00002-0000",
-                    "country": "United States"
-                }]
-            }, {
-                "type": "medicare msp:End stage Renal Disease (ESRD)",
-                "policy_number": "12345678901",
-                "payer_name": "Insurer2",
-                "date": [{
-                    "date": "2010-01-01T00:00:00Z",
-                    "precision": "day"
-                }, {
-                    "date": "2010-12-31T00:00:00Z",
-                    "precision": "day"
-                }],
-                "addresses": [{
-                    "use": "primary",
-                    "streetLines": [
-                        "0000 Any ROAD"
-                    ],
-                    "city": "ANYWHERE",
-                    "state": "VA",
-                    "zip": "00000-0000",
-                    "country": "United States"
-                }]
-            }, {
-                "type": "medicare msp",
-                "policy_number": "00001",
-                "payer_name": "Insurer",
-                "date": [{
-                    "date": "1984-10-01T00:00:00Z",
-                    "precision": "day"
-                }, {
-                    "date": "2008-11-30T00:00:00Z",
-                    "precision": "day"
-                }],
-                "addresses": [{
-                    "use": "primary",
-                    "streetLines": [
-                        "00 Address STREET"
-                    ],
-                    "city": "ANYWHERE",
-                    "state": "PA",
-                    "zip": "00000",
-                    "country": "United States"
-                }]
             }];
 
                 callback(null, tmpInsurance);
