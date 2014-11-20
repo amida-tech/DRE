f074f2dddd369590eeb2902e17c241fc6a564a04
Ekavali Mishra <ekavali@amida-tech.com>
2014-11-20 15:21:22 -0500

    billing navigation

---
+++ app/scripts/directives/billingnavigation.js
@@ -0,0 +1,20 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name phrPrototypeApp.directive:billingnavigation
+ * @description
+ * # billingnavigation
+ */
+angular.module('phrPrototypeApp')
+    .directive('billingnavigation', function() {
+        return {
+            template: '<div class="btn-group-vertical col-sm-12">' +
+                '<a href="#/billing/insurance" class="btn btn-default btn-block">Insurance</a>' +
+                '<a href="#/billing/claims" class="btn btn-default btn-block">Claims</a>' +
+                '</div>',
+            restrict: 'EA',
+            link: function postLink(scope, element, attrs) {;
+            }
+        };
+    });
