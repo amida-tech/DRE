c83294095a7e1b7d939458342839bb5d5bf9dd95
Matthew McCall <matt@mattmccall.com>
2014-11-26 07:04:06 -0800

    a few more fixes.

--- app/scripts/directives/recordnavigation.js
+++ app/scripts/directives/recordnavigation.js
@@ -9,16 +9,19 @@
 angular.module('phrPrototypeApp')
   .directive('recordNavigation', function ($location) {
     return {
-      template: '<div class="btn-group-vertical col-sm-12 record-navigation">' +
-      			'<a href="#/record/allergies" id="navallergies" class="btn btn-default btn-block">Allergies</a>' + 
-      			'<a href="#/record/encounters" id="navencounters" class="btn btn-default btn-block">Encounters</a>' +
-      			'<a href="#/record/immunizations" id="navimmunizations" class="btn btn-default btn-block">Immunizations</a>' +
-      			'<a href="#/record/medications" id="navmedications" class="btn btn-default btn-block">Medications</a>' +
-      			'<a href="#/record/conditions" id="navconditions" class="btn btn-default btn-block">Conditions</a>' +
-      			'<a href="#/record/procedures" id="navprocedures" class="btn btn-default btn-block">Procedures</a>' +
-      			'<a href="#/record/vitals" id="navvitals" class="btn btn-default btn-block">Vital Signs</a>' +
-      			'<a href="#/record/results" id="navresults" class="btn btn-default btn-block">Test Results</a>' +
-      			'<a href="#/record/social" id="navsocial" class="btn btn-default btn-block">Social History</a>' +
+      template: '<div class="col-sm-12 ">' +
+            '<div class="">' +
+            '<ul class="nav ">' +
+      			'<li><a href="#/record/allergies" id="navallergies" class="">Allergies</a></li>' + 
+      			'<li><a href="#/record/encounters" id="navencounters" class="">Encounters</a></li>' +
+      			'<li><a href="#/record/immunizations" id="navimmunizations" class="">Immunizations</a></li>' +
+      			'<li><a href="#/record/medications" id="navmedications" class="">Medications</a></li>' +
+      			'<li><a href="#/record/conditions" id="navconditions" class="">Conditions</a></li>' +
+      			'<li><a href="#/record/procedures" id="navprocedures" class="">Procedures</a></li>' +
+      			'<li><a href="#/record/vitals" id="navvitals" class="">Vital Signs</a></li>' +
+      			'<li><a href="#/record/results" id="navresults" class="">Test Results</a></li>' +
+      			'<li><a href="#/record/social" id="navsocial" class="">Social History</a></li>' +
+            '</ul>' +
       			'</div>',
       restrict: 'EA',
       link: function postLink(scope, element, attrs) {
