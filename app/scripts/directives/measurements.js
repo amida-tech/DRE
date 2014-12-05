e3b294e5e56613390bc2844bba34d64b64fc4c28
Jamie Johnson <jamiesanerd@gmail.com>
2014-12-05 16:26:07 -0500

    better chart
    - chart now loading in angular way
    - added actual data points

--- app/scripts/directives/measurements.js
+++ app/scripts/directives/measurements.js
@@ -25,7 +25,13 @@ angular.module('phrPrototypeApp').directive('d3template', ['$window', '$timeout'
                         w = 300 - margin.left - margin.right,
                         h = 200 - margin.top - margin.bottom;
                     //set initial svg values
-                    var svg = d3.select(ele[0]).append('svg').attr('class', 'chart').attr('width', w + margin.left + margin.right).attr('height', h + margin.top + margin.bottom).append('g').attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+                    var svg = d3.select(ele[0])
+                        .append('svg')
+                        .attr('class', 'chart')
+                        .attr('width', w + margin.left + margin.right)
+                        .attr('height', h + margin.top + margin.bottom)
+                        .append('g')
+                        .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
                     //watch window resize to re-render
                     $window.onresize = function() {
                         scope.$apply();
@@ -204,7 +210,7 @@ angular.module('phrPrototypeApp').directive('d3template', ['$window', '$timeout'
                         scope.render(newVals);
                     }, true);
                     //called to render chart
-                    scope.render = function(data) {
+                    scope.render = function(vitals) {
                         //clean svg
                         svg.selectAll('*').remove();
                         //check to see if there is any data before drawing the chart (enable when using data attribute)
@@ -221,7 +227,104 @@ angular.module('phrPrototypeApp').directive('d3template', ['$window', '$timeout'
                                 svg.attr('width', w + margin.left + margin.right).attr('height', h + margin.top + margin.bottom);
                             }
                             //write your chart code here!
-                            svg.append('rect').attr('width', w).attr('height', h).attr('fill', 'black');
+                            var graphVitals = [];
+                            var graphVitalsTwo = [];
+                            var graphDates = [];
+                            var graphValues = [];
+                            var format = d3.time.format("%Y-%m-%dT%H:%M:%SZ");
+                            _.each(vitals, function(entry) {
+                                var tmpVital = {};
+                                if (attrs.graphType === "weight") {
+                                    if (entry.data.vital.name === "Patient Body Weight - Measured") {
+                                        tmpVital.value = entry.data.value;
+                                        tmpVital.unit = entry.data.unit;
+                                        tmpVital.date = format.parse(entry.data.date_time.point.date);
+                                        graphVitals.push(tmpVital);
+                                        graphDates.push(tmpVital.date);
+                                        graphValues.push(tmpVital.value);
+                                    }
+                                }
+                                if (attrs.graphType === "bloodPressure") {
+                                    if (entry.data.vital.name === "Intravascular Diastolic") {
+                                        tmpVital.value = entry.data.value;
+                                        tmpVital.unit = entry.data.unit;
+                                        tmpVital.date = format.parse(entry.data.date_time.point.date);
+                                        graphVitals.push(tmpVital);
+                                        graphDates.push(tmpVital.date);
+                                        graphValues.push(tmpVital.value);
+                                    }
+                                    if (entry.data.vital.name === "Intravascular Systolic") {
+                                        tmpVital.value = entry.data.value;
+                                        tmpVital.unit = entry.data.unit;
+                                        tmpVital.date = format.parse(entry.data.date_time.point.date);
+                                        graphVitalsTwo.push(tmpVital);
+                                        graphDates.push(tmpVital.date);
+                                        graphValues.push(tmpVital.value);
+                                    }
+                                }
+                            });
+                            var padding = 30;
+                            var pathClass = "path";
+                            var xScale, yScale, xAxisGen, yAxisGen, lineFun;
+
+                            function setChartParameters() {
+                                xScale = d3.time.scale().domain([d3.min(graphDates), d3.max(graphDates)]).range([margin.left - 2, w - margin.right - 2]);
+                                yScale = d3.scale.linear().domain([d3.min(graphValues) - (d3.min(graphValues) * 0.1), d3.max(graphValues) + (d3.max(graphValues) * 0.1)]).range([h - margin.top, margin.bottom]);
+                                xAxisGen = d3.svg.axis().scale(xScale).orient("bottom").tickFormat(d3.time.format("%m/%d")).outerTickSize([2]).tickValues([d3.min(graphDates), d3.max(graphDates)]);
+                                yAxisGen = d3.svg.axis().scale(yScale).orient("left").ticks(4).outerTickSize([2]);
+                                lineFun = d3.svg.line().x(function(d) {
+                                    return xScale(d.date);
+                                }).y(function(d) {
+                                    return yScale(d.value);
+                                }).interpolate("cardinal").tension(0.5);
+                            }
+
+                            function drawLineChart() {
+                                setChartParameters();
+                                svg.append("svg:g").attr("class", "x axis").attr("transform", "translate(0," + (h - (margin.top)) + ")").call(xAxisGen);
+                                svg.append("svg:g").attr("class", "y axis").attr("transform", "translate(" + (margin.right) + ",0)").call(yAxisGen);
+                                
+
+
+                                if (graphVitals.length > 0) {
+                                    svg.append("svg:path").attr({
+                                        d: lineFun(graphVitals),
+                                        "stroke": "blue",
+                                        "stroke-width": 2,
+                                        "fill": "none",
+                                        "class": pathClass
+                                    });
+                                    svg.selectAll("dot")
+                                        .data(graphVitals)
+                                    .enter().append("circle")
+                                        .attr("r",4.5)
+                                        .attr("cx", function(d) { return xScale(d.date);})
+                                        .attr("cy", function(d) { return yScale(d.value);});
+                                }
+                                if (graphVitalsTwo.length > 0) {
+                                    svg.append("svg:path").attr({
+                                        d: lineFun(graphVitalsTwo),
+                                        "stroke": "blue",
+                                        "stroke-width": 2,
+                                        "fill": "none",
+                                        "class": pathClass
+                                    });
+                                    svg.selectAll("dot")
+                                        .data(graphVitalsTwo)
+                                    .enter().append("circle")
+                                        .attr("r",4.5)
+                                        .attr("cx", function(d) { return xScale(d.date);})
+                                        .attr("cy", function(d) { return yScale(d.value);});
+                                }
+                                svg.selectAll("circle")
+                                    .style('fill','#fff')
+                                    .style('stroke','#000');
+
+                            }
+                            drawLineChart();
+
+
+
                         }, 100);
                     };
                 });
