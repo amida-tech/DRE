af6b5132f1f68a67903424f066aa722b6e27f27c
Ekavali Mishra <ekavali@amida-tech.com>
2014-12-08 10:16:17 -0500

    review encounters
    header

--- app/scripts/controllers/record/encounters/review.js
+++ app/scripts/controllers/record/encounters/review.js
@@ -8,10 +8,156 @@
  * Controller of the phrPrototypeApp
  */
 angular.module('phrPrototypeApp')
-  .controller('RecordEncountersReviewCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('RecordEncountersReviewCtrl', function ($scope, $location, encounters, format) {
+        $scope.entryType = 'encounters';
+        $scope.masterEntries = [];
+        $scope.entries = [];
+        $scope.updateDate = null;
+        $scope.inactiveFlag = false;
+        $scope.partialEntries = [];
+        $scope.alertShow = true;
+        $scope.match = [];
+
+        //Instantiate empty clone of object structure.
+        var selectedOriginal = {
+            "observation": {
+                "status": null,
+                "reactions": {}
+            }
+        };
+
+        $scope.selected = angular.copy(selectedOriginal);
+
+        $scope.closeAlert = function () {
+            $scope.alertShow = false;
+        }
+
+        function getRecords(callback) {
+            encounters.getRecord(function (err, results) {
+                $scope.masterEntries = results;
+                $scope.entries = results;
+                callback();
+            });
+        }
+
+        function getPartial(callback) {
+            encounters.getPartialRecord(function (err, results) {
+                $scope.entries = results;
+                callback();
+            });
+        }
+
+        function getMatch(callback) {
+            encounters.getPartialMatch(function (err, results) {
+                $scope.match = results;
+
+                //Duplicate originals for restore.
+                _.each($scope.match, function (match) {
+                    match.origMatch = angular.copy(match.srcMatch);
+                })
+
+                callback();
+            });
+        }
+
+        function formatDates() {
+            //Add displayDate to all entries.
+            _.each($scope.match, function (entry) {
+
+                //Format newMatch Date Display.
+                _.each(entry.newMatch.date_time, function (dateEntry) {
+                    format.formatDate(dateEntry);
+                });
+                entry.newMatch.date_time.displayDate = format.outputDate(entry.newMatch.date_time);
+
+                //Format srcMatch Date Display.
+                _.each(entry.srcMatch.date_time, function (dateEntry) {
+                    format.formatDate(dateEntry);
+                });
+                entry.srcMatch.date_time.displayDate = format.outputDate(entry.srcMatch.date_time);
+
+            });
+        }
+
+        $scope.selectEntry = function (matchIndex) {
+            _.each($scope.selected, function (elem, name, list) {
+
+                if (elem === true) {
+                    $scope.match[matchIndex].srcMatch[name] = $scope.match[matchIndex].newMatch[name];
+                } else if (elem === false) {
+                    $scope.match[matchIndex].srcMatch[name] = $scope.match[matchIndex].origMatch[name];
+                    formatDates();
+                } else if (_.isObject(elem)) {
+                    // Handle status
+                    if (elem.status === true) {
+                        $scope.match[matchIndex].srcMatch[name].status = $scope.match[matchIndex].newMatch[name].status;
+                    } else if (elem.status === false) {
+                        $scope.match[matchIndex].srcMatch[name].status = $scope.match[matchIndex].origMatch[name].status;
+                    }
+                    //Explicitly handle reactions.
+                    _.each(elem.reactions, function (rElem, rName, rList) {
+
+                        if (rElem === true) {
+
+                            var newReaction = $scope.match[matchIndex].newMatch[name].reactions[rName];
+                            newReaction.srcMatchIndex = rName;
+
+                            if (!$scope.match[matchIndex].srcMatch[name].reactions) {
+                                $scope.match[matchIndex].srcMatch[name].reactions = [];
+                            }
+
+                            $scope.match[matchIndex].srcMatch[name].reactions.push(newReaction);
+                        } else if (rElem === false) {
+
+                            //Index to splice.
+                            //console.log(rName);
+
+                            _.each($scope.match[matchIndex].srcMatch[name].reactions, function (rxElem, rxName, rxList) {
+
+                                if (rxElem.srcMatchIndex) {
+                                    if (rxElem.srcMatchIndex === rName) {
+                                        //Splice ID.
+                                        console.log(rxName);
+                                        $scope.match[matchIndex].srcMatch[name].reactions.splice(rxName);
+                                    }
+                                }
+                            });
+                        }
+
+                    });
+
+                }
+
+            });
+
+        }
+
+        $scope.clearAll = function (matchIndex) {
+
+            $scope.selected = angular.copy(selectedOriginal);  
+            $scope.match[matchIndex].srcMatch = angular.copy($scope.match[matchIndex].origMatch);
+            formatDates();
+
+        }
+
+        $scope.saveUpdate = function (matchIndex) {
+
+            console.log($scope.match[matchIndex].srcMatch);
+
+            allergies.saveEntry($scope.match[matchIndex].srcMatch, function (err) {
+
+                $location.path('/record/allergies');
+
+
+            });
+
+        }
+
+        $scope.refresh = function () {
+            getMatch(function (err) {
+                formatDates();
+            });
+        }
+
+        $scope.refresh();
   });
--- app/scripts/services/record/encounters.js
+++ app/scripts/services/record/encounters.js
@@ -10,7 +10,81 @@
 angular.module('phrPrototypeApp')
     .service('encounters', function encounters() {
 
-        this.getRecord = function (callback) {
+
+        var getPartialRecord = function (callback) {
+            var tmpEncounters = [{
+                "encounter": {
+                    "name": "Office outpatient visit 15 minutes",
+                    "code": "99213",
+                    "code_system_name": "CPT",
+                    "translations": [{
+                        "name": "Ambulatory",
+                        "code": "AMB",
+                        "code_system_name": "HL7ActCode"
+                    }]
+                },
+                "identifiers": [{
+                    "identifier": "2a620155-9d11-439e-92b3-5d9815ff4de8"
+                }],
+                "date_time": {
+                    "point": {
+                        "date": "2009-02-27T13:00:00Z",
+                        "precision": "second"
+                    }
+                },
+                "performers": [{
+                    "identifiers": [{
+                        "identifier": "PseduoMD-3"
+                    }],
+                    "code": [{
+                        "name": "General Physician",
+                        "code": "59058001",
+                        "code_system_name": "SNOMED CT"
+                    }]
+                }],
+                "locations": [{
+                    "name": "Community Urgent Care Center",
+                    "location_type": {
+                        "name": "Urgent Care Center",
+                        "code": "1160-1",
+                        "code_system_name": "HealthcareServiceLocation"
+                    },
+                    "address": [{
+                        "street_lines": [
+                            "17 Daws Rd."
+                        ],
+                        "city": "Blue Bell",
+                        "state": "MA",
+                        "zip": "02368",
+                        "country": "US"
+                    }]
+                }],
+                "findings": [{
+                    "identifiers": [{
+                        "identifier": "db734647-fc99-424c-a864-7e3cda82e703",
+                        "extension": "45665"
+                    }],
+                    "value": {
+                        "name": "Pneumonia",
+                        "code": "233604007",
+                        "code_system_name": "SNOMED CT"
+                    },
+                    "date_time": {
+                        "low": {
+                            "date": "2007-01-03T00:00:00Z",
+                            "precision": "day"
+                        }
+                    }
+                }]
+            }];
+
+            callback(null, tmpEncounters);
+
+        }
+
+        this.getPartialRecord = getPartialRecord;
+
+        var getRecord = function (callback) {
 
             var tmpEncounters = [{
                 "encounter": {
@@ -81,4 +155,54 @@ angular.module('phrPrototypeApp')
             callback(null, tmpEncounters);
 
         }
+
+        this.getRecord = getRecord;
+
+        var saveEntry = function (entry, callback) {
+            console.log(entry);
+            callback(null);
+        }
+
+    this.saveEntry = saveEntry;
+
+    var getPartialMatch = function (callback) {
+
+        getPartialRecord(function (err, partialResults) {
+            getRecord(function (err, recordResults) {
+
+                var tmpMatch = [{
+                    "match": "partial",
+                    "percent": 75,
+                    "subelements": {
+                        "observation": {
+                            "reactions": [{
+                                "match": "new",
+                                "percent": 0,
+                                "src_id": "0",
+                                "dest_id": "0",
+                                "dest": "dest"
+                            }]
+                        }
+                    },
+                    "diff": {
+                        "observation": {
+                            "status": "diff",
+                            "reactions": "diff"
+                        }
+                    },
+                    "srcMatch": recordResults[0],
+                    "newMatch": partialResults[0]
+                }];
+
+                callback(null, tmpMatch);
+
+            });
+        });
+    }
+
+this.getPartialMatch = getPartialMatch;
+
     });
+
+
+
