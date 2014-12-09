dbe3a7e4058d46ffdc70655d8a6f9704f5ad0816
Ekavali Mishra <ekavali@amida-tech.com>
2014-12-09 12:46:06 -0500

    procedures

--- app/scripts/controllers/record/procedures/review.js
+++ app/scripts/controllers/record/procedures/review.js
@@ -8,10 +8,193 @@
  * Controller of the phrPrototypeApp
  */
 angular.module('phrPrototypeApp')
-  .controller('RecordProceduresReviewCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('RecordProceduresReviewCtrl', function ($scope, $location, procedures, format) {
+
+        $scope.entryType = 'procedures';
+        $scope.masterEntries = [];
+        $scope.entries = [];
+        $scope.updateDate = null;
+        $scope.partialEntries = [];
+        $scope.alertShow = true;
+        $scope.match = [];
+
+        //Instantiate empty clone of object structure.
+        var selectedOriginal = {
+            "performer": null
+        };
+
+        $scope.selected = angular.copy(selectedOriginal);
+
+        $scope.closeAlert = function () {
+            $scope.alertShow = false;
+        }
+
+        function getRecords(callback) {
+            procedures.getRecord(function (err, results) {
+                $scope.masterEntries = results;
+                $scope.entries = results;
+                callback();
+            });
+        }
+
+        function getPartial(callback) {
+            procedures.getPartialRecord(function (err, results) {
+                $scope.entries = results;
+                callback();
+            });
+        }
+
+        function getMatch(callback) {
+            procedures.getPartialMatch(function (err, results) {
+                $scope.match = results;
+
+                //Duplicate originals for restore.
+                _.each($scope.match, function (match) {
+                    match.origMatch = angular.copy(match.srcMatch);
+                })
+
+                callback();
+            });
+        }
+
+        function formatDates() {
+            //Add displayDate to all entries.
+            _.each($scope.match, function (entry) {
+
+                //Format newMatch Date Display.
+                _.each(entry.newMatch.date_time, function (dateEntry) {
+                    format.formatDate(dateEntry);
+                });
+                entry.newMatch.date_time.displayDate = format.outputDate(entry.newMatch.date_time);
+
+                //Format srcMatch Date Display.
+                _.each(entry.srcMatch.date_time, function (dateEntry) {
+                    format.formatDate(dateEntry);
+                });
+                entry.srcMatch.date_time.displayDate = format.outputDate(entry.srcMatch.date_time);
+
+            });
+        }
+
+        function formatDisplay() {
+        	_.each($scope.match, function(entry) {
+
+        		_.each(entry.newMatch.performer, function (perf) {
+
+        			_.each(perf.address, function(addr) {
+        				format.formatAddress(addr);
+        			});
+
+
+                	_.each(perf.organization, function(org) {
+
+                        _.each(org.address, function(orgAddr) {
+                            format.formatAddress(orgAddr);
+
+                        });
+
+                    });
+        		});
+
+        		_.each(entry.srcMatch.performer, function(perf) {
+
+        			_.each(perf.address, function(addr) {
+        				format.formatAddress(addr);
+        			});
+
+                	_.each(perf.organization, function(org) {
+
+                        _.each(org.address, function(orgAddr) {
+                            format.formatAddress(orgAddr);
+
+                        });
+
+                    });
+        		});
+        	});
+        }
+
+        $scope.selectEntry = function (matchIndex) {
+            _.each($scope.selected, function (elem, name, list) {
+
+                if (elem === true) {
+                    $scope.match[matchIndex].srcMatch[name] = $scope.match[matchIndex].newMatch[name];
+                } else if (elem === false) {
+                    $scope.match[matchIndex].srcMatch[name] = $scope.match[matchIndex].origMatch[name];
+                    formatDates();
+                } else if (_.isObject(elem)) {
+                    // Handle status
+                    if (elem.status === true) {
+                        $scope.match[matchIndex].srcMatch[name].status = $scope.match[matchIndex].newMatch[name].status;
+                    } else if (elem.status === false) {
+                        $scope.match[matchIndex].srcMatch[name].status = $scope.match[matchIndex].origMatch[name].status;
+                    }
+                    //Explicitly handle reactions.
+                    _.each(elem.reactions, function (rElem, rName, rList) {
+
+                        if (rElem === true) {
+
+                            var newReaction = $scope.match[matchIndex].newMatch[name].reactions[rName];
+                            newReaction.srcMatchIndex = rName;
+
+                            if (!$scope.match[matchIndex].srcMatch[name].reactions) {
+                                $scope.match[matchIndex].srcMatch[name].reactions = [];
+                            }
+
+                            $scope.match[matchIndex].srcMatch[name].reactions.push(newReaction);
+                        } else if (rElem === false) {
+
+                            //Index to splice.
+                            //console.log(rName);
+
+                            _.each($scope.match[matchIndex].srcMatch[name].reactions, function (rxElem, rxName, rxList) {
+
+                                if (rxElem.srcMatchIndex) {
+                                    if (rxElem.srcMatchIndex === rName) {
+                                        //Splice ID.
+                                        console.log(rxName);
+                                        $scope.match[matchIndex].srcMatch[name].reactions.splice(rxName);
+                                    }
+                                }
+                            });
+                        }
+
+                    });
+
+                }
+
+            });
+
+        }
+
+        $scope.clearAll = function (matchIndex) {
+
+            $scope.selected = angular.copy(selectedOriginal);  
+            $scope.match[matchIndex].srcMatch = angular.copy($scope.match[matchIndex].origMatch);
+            formatDates();
+            formatDisplay();
+
+        }
+
+        $scope.saveUpdate = function (matchIndex) {
+
+            console.log($scope.match[matchIndex].srcMatch);
+
+            procedures.saveEntry($scope.match[matchIndex].srcMatch, function (err) {
+
+                $location.path('/record/procedures');
+
+
+            });
+
+        }
+
+        $scope.refresh = function () {
+            getMatch(function (err) {
+                formatDates();
+                formatDisplay();
+            });
+        }
+
+        $scope.refresh();
   });
--- app/scripts/services/record/procedures.js
+++ app/scripts/services/record/procedures.js
@@ -10,7 +10,89 @@
 angular.module('phrPrototypeApp')
     .service('procedures', function procedures() {
 
-        this.getRecord = function (callback) {
+        var getPartialRecord = function(callback) {
+
+            var tmpProcedures = [{
+                "procedure": {
+                    "name": "Colonoscopy",
+                    "code": "73761001",
+                    "code_system_name": "SNOMED CT"
+                },
+                "identifiers": [{
+                    "identifier": "d68b7e32-7810-4f5b-9cc2-acd54b0fd85d"
+                }],
+                "status": "Completed",
+                "date_time": {
+                    "point": {
+                        "date": "2012-05-12T00:00:00Z",
+                        "precision": "day"
+                    }
+                },
+                "body_sites": [{
+                    "name": "colon",
+                    "code": "appropriate_code",
+                    "code_system_name": "OID 2.16.840.1.113883.3.88.12.3221.8.9"
+                }],
+                "specimen": {
+                    "identifiers": [{
+                        "identifier": "c2ee9ee9-ae31-4628-a919-fec1cbb58683"
+                    }],
+                    "code": {
+                        "name": "colonic polyp sample",
+                        "code": "309226005",
+                        "code_system_name": "SNOMED CT"
+                    }
+                },
+                "performer": [{
+                    "identifiers": [{
+                        "identifier": "2.16.840.1.113883.19.5.9999.456",
+                        "extension": "2981823"
+                    }],
+                    "address": [{
+                        "street_lines": [
+                            "1002 Village Avenue"
+                        ],
+                        "city": "Portland",
+                        "state": "OR",
+                        "zip": "99123",
+                        "country": "US"
+                    }],
+                    "phone": [{
+                        "number": "555-555-5000",
+                        "type": "work place"
+                    }],
+                    "organization": [{
+                        "identifiers": [{
+                            "identifier": "2.16.840.1.113883.19.5.9999.1393"
+                        }],
+                        "name": [
+                            "Community Health and Hospitals"
+                        ],
+                        "address": [{
+                            "street_lines": [
+                                "1001 Village Avenue"
+                            ],
+                            "city": "Portland",
+                            "state": "OR",
+                            "zip": "99123",
+                            "country": "US"
+                        }],
+                        "phone": [{
+                            "number": "555-555-5000",
+                            "type": "work place"
+                        }]
+                    }]
+                }],
+                "procedure_type": "procedure"
+            }];
+
+            callback(null, tmpProcedures);
+
+        }
+
+        this.getPartialRecord = getPartialRecord;
+
+        var getRecord = function (callback) {
 
             var tmpProcedures = [{
                 "procedure": {
@@ -87,6 +169,42 @@ angular.module('phrPrototypeApp')
             }];
 
             callback(null, tmpProcedures);
-
         }
+
+        this.getRecord = getRecord;
+
+        var saveEntry = function(entry, callback) {
+            console.log(entry);
+            callback(null);
+        }
+
+        this.saveEntry = saveEntry;
+
+        var getPartialMatch = function(callback) {
+
+            getPartialRecord(function(err, partialResults) {
+                getRecord(function(err, recordResults) {
+
+                    var tmpMatch = [{
+                        "match": "partial",
+                        "percent": 75,
+                        "subelements": {
+                        },
+                        "diff": {
+                            "performer": "diff",
+                        },
+                        "srcMatch": recordResults[0],
+                        "newMatch": partialResults[0]
+                    }];
+
+                    callback(null, tmpMatch);
+
+                });
+            });
+        }
+
+        this.getPartialMatch = getPartialMatch;
+
+
     });
+
