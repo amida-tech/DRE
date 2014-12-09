e23671db604b96edad62332cefb27265aae18c7b
Ekavali Mishra <ekavali@amida-tech.com>
2014-12-09 02:19:05 -0500

    Conditions

--- app/scripts/controllers/record/conditions/review.js
+++ app/scripts/controllers/record/conditions/review.js
@@ -8,10 +8,157 @@
  * Controller of the phrPrototypeApp
  */
 angular.module('phrPrototypeApp')
-  .controller('RecordConditionsReviewCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('RecordConditionsReviewCtrl', function ($scope, $location, conditions, format) {
+
+        $scope.entryType = 'conditions';
+        $scope.masterEntries = [];
+        $scope.entries = [];
+        $scope.updateDate = null;
+        $scope.inactiveFlag = false;
+        $scope.partialEntries = [];
+        $scope.alertShow = true;
+        $scope.match = [];
+
+        //Instantiate empty clone of object structure.
+        var selectedOriginal = {
+            "problem": null
+        };
+
+        $scope.selected = angular.copy(selectedOriginal);
+
+        $scope.closeAlert = function () {
+            $scope.alertShow = false;
+        }
+
+        function getRecords(callback) {
+            conditions.getRecord(function (err, results) {
+                $scope.masterEntries = results;
+                $scope.entries = results;
+                callback();
+            });
+        }
+
+        function getPartial(callback) {
+            conditions.getPartialRecord(function (err, results) {
+                $scope.entries = results;
+                callback();
+            });
+        }
+
+        function getMatch(callback) {
+            conditions.getPartialMatch(function (err, results) {
+                $scope.match = results;
+
+                //Duplicate originals for restore.
+                _.each($scope.match, function (match) {
+                    match.origMatch = angular.copy(match.srcMatch);
+                })
+
+                callback();
+            });
+        }
+
+        function formatDates() {
+            //Add displayDate to all entries.
+            _.each($scope.match, function (entry) {
+                	//newMatch
+                    _.each(entry.newMatch.date_time, function (dateEntry) {
+                        format.formatDate(dateEntry);
+                    });
+                    entry.newMatch.date_time.displayDate = format.outputDate(entry.newMatch.date_time);
+                    entry.newMatch.date_time.plotDate = format.plotDate(entry.newMatch.date_time);
+                
+                	//srcMatch
+                    _.each(entry.srcMatch.date_time, function (dateEntry) {
+                        format.formatDate(dateEntry);
+                    });
+                    entry.srcMatch.date_time.displayDate = format.outputDate(entry.srcMatch.date_time);
+                    entry.srcMatch.date_time.plotDate = format.plotDate(entry.srcMatch.date_time);
+                
+            });
+        }
+
+
+        $scope.selectEntry = function (matchIndex) {
+            _.each($scope.selected, function (elem, name, list) {
+
+                if (elem === true) {
+                    $scope.match[matchIndex].srcMatch[name] = $scope.match[matchIndex].newMatch[name];
+                } else if (elem === false) {
+                    $scope.match[matchIndex].srcMatch[name] = $scope.match[matchIndex].origMatch[name];
+                    formatDates();
+                } else if (_.isObject(elem)) {
+                    // Handle status
+                    if (elem.status === true) {
+                        $scope.match[matchIndex].srcMatch[name].status = $scope.match[matchIndex].newMatch[name].status;
+                    } else if (elem.status === false) {
+                        $scope.match[matchIndex].srcMatch[name].status = $scope.match[matchIndex].origMatch[name].status;
+                    }
+                    //Explicitly handle reactions.
+                    _.each(elem.reactions, function (rElem, rName, rList) {
+
+                        if (rElem === true) {
+
+                            var newReaction = $scope.match[matchIndex].newMatch[name].reactions[rName];
+                            newReaction.srcMatchIndex = rName;
+
+                            if (!$scope.match[matchIndex].srcMatch[name].reactions) {
+                                $scope.match[matchIndex].srcMatch[name].reactions = [];
+                            }
+
+                            $scope.match[matchIndex].srcMatch[name].reactions.push(newReaction);
+                        } else if (rElem === false) {
+
+                            //Index to splice.
+                            //console.log(rName);
+
+                            _.each($scope.match[matchIndex].srcMatch[name].reactions, function (rxElem, rxName, rxList) {
+
+                                if (rxElem.srcMatchIndex) {
+                                    if (rxElem.srcMatchIndex === rName) {
+                                        //Splice ID.
+                                        console.log(rxName);
+                                        $scope.match[matchIndex].srcMatch[name].reactions.splice(rxName);
+                                    }
+                                }
+                            });
+                        }
+
+                    });
+
+                }
+
+            });
+
+        }
+
+        $scope.clearAll = function (matchIndex) {
+
+            $scope.selected = angular.copy(selectedOriginal);  
+            $scope.match[matchIndex].srcMatch = angular.copy($scope.match[matchIndex].origMatch);
+            formatDates();
+
+        }
+
+        $scope.saveUpdate = function (matchIndex) {
+
+            console.log($scope.match[matchIndex].srcMatch);
+
+            conditions.saveEntry($scope.match[matchIndex].srcMatch, function (err) {
+
+                $location.path('/record/conditions');
+
+
+            });
+
+        }
+
+        $scope.refresh = function () {
+            getMatch(function (err) {
+                formatDates();
+                // formatDisplay();
+            });
+        }
+
+        $scope.refresh();
   });
--- app/scripts/services/record/conditions.js
+++ app/scripts/services/record/conditions.js
@@ -10,9 +10,57 @@
 angular.module('phrPrototypeApp')
     .service('conditions', function conditions() {
 
-        this.getRecord = function (callback) {
+        var getPartialRecord = function (callback) {
 
             var tmpConditions = [{
+                "date_time": {
+                    "low": {
+                        "date": "2008-01-03T00:00:00Z",
+                        "precision": "day"
+                    },
+                    "high": {
+                        "date": "2008-01-03T00:00:00Z",
+                        "precision": "day"
+                    }
+                },
+                "identifiers": [{
+                    "identifier": "ab1791b0-5c71-11db-b0de-0800200c9a66"
+                }],
+                "negation_indicator": false,
+                "problem": {
+                    "code": {
+                        "name": "Pneumonia"
+                    }
+                },
+                "onset_age": "57",
+                "onset_age_unit": "Year",
+                "status": {
+                    "name": "Resolved",
+                    "date_time": {
+                        "low": {
+                            "date": "2008-01-03T00:00:00Z",
+                            "precision": "day"
+                        },
+                        "high": {
+                            "date": "2009-02-27T13:00:00Z",
+                            "precision": "second"
+                        }
+                    }
+                },
+                "patient_status": "Alive and well",
+                "source_list_identifiers": [{
+                    "identifier": "ec8a6ff8-ed4b-4f7e-82c3-e98e58b45de7"
+                }]
+            }];
+
+            callback(null, tmpConditions);
+
+        }
+
+        this.getPartialRecord = getPartialRecord;
+
+        var getRecord = function (callback) {
+           var tmpConditions = [{
                 "date_time": {
                     "low": {
                         "date": "2008-01-03T00:00:00Z",
@@ -68,5 +116,38 @@ angular.module('phrPrototypeApp')
             callback(null, tmpConditions);
 
         }
+
+        this.getRecord = getRecord;
+
+        var saveEntry = function (entry, callback) {
+        console.log(entry);
+        callback(null);
+    }
+
+    this.saveEntry = saveEntry;
+
+        var getPartialMatch = function (callback) {
+
+        getPartialRecord(function (err, partialResults) {
+            getRecord(function (err, recordResults) {
+
+                var tmpMatch = [{
+                    "match": "partial",
+                    "percent": 75,
+                    "subelements": {},
+                    "diff": {
+                        "problem":"diff"
+                    },
+                    "srcMatch": recordResults[0],
+                    "newMatch": partialResults[0]
+                }];
+
+                callback(null, tmpMatch);
+
+            });
+        });
+}
+
+this.getPartialMatch = getPartialMatch;
 
     });
