5bc8386dbccdc2a2f3b7476f75b91d7d36ad7730
Matthew McCall <matt@mattmccall.com>
2014-12-11 13:43:23 -0800

    Clean up landing a bit.

--- app/scripts/controllers/home.js
+++ app/scripts/controllers/home.js
@@ -36,9 +36,11 @@ angular.module('phrPrototypeApp')
 
           for (var i in history.recordHistory) {
             if (history.recordHistory[i].type === 'upload') {
-              history.recordHistory[i].displayType = 'New File Uploaded';
+              history.recordHistory[i].displayType = 'New Record Uploaded';
             } else if (history.recordHistory[i].type === 'download') {
-              history.recordHistory[i].displayType = 'File Downloaded';
+              history.recordHistory[i].displayType = 'Record Downloaded';
+            } else if (history.recordHistory[i].type === 'login') {
+              history.recordHistory[i].displayType = 'Logged In';
             }
 
             console.log();
