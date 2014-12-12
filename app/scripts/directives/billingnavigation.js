f9b0447c1dad9c392cbf34ac1ce825d0e77c41ee
Matthew McCall <matt@mattmccall.com>
2014-12-12 09:31:44 -0800

    fix billing highlighting.

--- app/scripts/directives/billingnavigation.js
+++ app/scripts/directives/billingnavigation.js
@@ -10,12 +10,21 @@ angular.module('phrPrototypeApp')
     .directive('billingnavigation', function() {
         return {
             template: '<div class="btn-group-vertical col-sm-12 sidebar-control">' +
-                '<a href="#/billing/insurance" class="btn btn-info btn-block">Insurance</a>' +
-                '<a href="#/billing/claims" class="btn btn-info btn-block">Claims</a>' +
+                '<a href="#/billing/insurance" class="btn btn-info btn-block" ng-class="{active: entryInsurance}">Insurance</a>' +
+                '<a href="#/billing/claims" class="btn btn-info btn-block" ng-class="{active: entryClaims}">Claims</a>' +
                 '</div>',
             restrict: 'EA',
             link: function postLink(scope, element, attrs) {
-            	//$.lockfixed(".sidebar-control",{offset: {top: 10},forcemargin: true});
+
+                scope.entryInsurance = false;
+                scope.entryClaims = false;
+
+                if (scope.entryType === 'insurance') {
+                    scope.entryInsurance = true;
+                } else if (scope.entryType === 'claims') {
+                    scope.entryClaims = true;
+                }
+                
             }
         };
     });
