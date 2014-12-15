b4ce0d26d92acc360cb7fe5e6d407c8b9e12f090
Matthew McCall <matt@mattmccall.com>
2014-12-15 10:55:16 -0800

    remove lock fix, add image hashes.

--- app/scripts/controllers/bonus.js
+++ app/scripts/controllers/bonus.js
@@ -9,10 +9,10 @@
 angular.module('phrPrototypeApp').controller('bonusCtrl', function($scope, $location) {
     $(window).konami({
         cheat: function() {
-            document.getElementById("index-wrapper").style.backgroundImage = 'url(../../images/va_photo.jpg)';
+            document.getElementById("index-wrapper").style.backgroundImage = 'url(../../images/va_photo.66fe66c9.jpg)';
             document.getElementById("index-wrapper").style.backgroundRepeat = 'repeat';
             setTimeout(function() {
-                document.getElementById("index-wrapper").style.backgroundImage = 'url(../../images/blur.png)';
+                document.getElementById("index-wrapper").style.backgroundImage = 'url(../../images/blur.00ed6507.png)';
                 document.getElementById("index-wrapper").style.backgroundRepeat = 'no repeat';
             }, 2000);
         }
