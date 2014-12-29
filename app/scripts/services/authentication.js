7c7220fc3fb76954d6f6b1423156ebde6c23d40b
Dmitry Kachaev <dmitry.kachaev@gmail.com>
2014-12-29 10:04:56 -0500

    login/logout/auth status API calls wired

--- app/scripts/services/authentication.js
+++ app/scripts/services/authentication.js
@@ -8,41 +8,71 @@
  * Service in the phrPrototypeApp.
  */
 angular.module('phrPrototypeApp')
-    .service('authentication', function authentication($location) {
+    .service('authentication', function authentication($location, $http) {
 
 
         //TODO:  Hygiene here for max length of inputs.
-        this.login = function (username, password, callback) {
+        this.login = function(username, password, callback) {
             if (username && password) {
+
+                $http.post('api/v1/login', {
+                        username: username,
+                        password: password
+                    })
+                    .success(function(data) {
+                        //$location.path('/dashboard');
+                        callback(null);
+                    }).error(function(data) {
+                        //callback(data);
+                        callback('Invalid Login and/or Password.');
+                    });
+
                 //Stubbed login.
-                if ((username === 'test' && password === 'test') || (username === 'test@amida-demo.com' && password === 'test')) {
+                /*(if ((username === 'test' && password === 'test') || (username === 'test@amida-demo.com' && password === 'test')) {
                     callback(null);
                 } else {
                     callback('Invalid Login and/or Password.');
-                }
+                }*/
             }
         };
 
-        this.logout = function (callback) {
-            
-        	var err = null;
+        this.logout = function(callback) {
 
-        	//Stubbed logout.
-        	if (err) {
-        		callback(err);
-        	} else {
-        		callback(null);
-        	}
+            var err = null;
+
+
+            $http.post('api/v1/logout')
+                .success(function() {
+                    //$rootScope.isAuthenticated = false;
+                    //$location.path('/home');
+                    callback(null);
+                }).error(function(err) {
+                    callback(err);
+                });
+
+            //Stubbed logout.
+            /*if (err) {
+                callback(err);
+            } else {
+                callback(null);
+            }*/
         };
 
         //This would be a server call, but now just stubbed with $location.
-        this.authStatus = function (callback) {
+        this.authStatus = function(callback) {
 
-            if ($location.path() === "/" || $location.path() === "/login" || $location.path() === "/register" || $location.path() === "/reset") {
+            $http.get('api/v1/account')
+                .success(function() {
+                    callback(null, true);
+                }).error(function(err) {
+                    callback(err, false);
+                });
+
+            /*if ($location.path() === "/" || $location.path() === "/login" || $location.path() === "/register" || $location.path() === "/reset") {
                 callback(null, false);
             } else {
                 callback(null, true);
-            }
+            }*/
 
         };
 
