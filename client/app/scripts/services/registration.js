c1e5c5cf1536f02d9f2cdf039f831e0332eb46ad
Dmitry Kachaev <dmitry.kachaev@gmail.com>
2015-01-23 14:22:57 -0500

    registration js and backend login fixes

--- client/app/scripts/services/registration.js
+++ client/app/scripts/services/registration.js
@@ -13,20 +13,29 @@ angular.module('phrPrototypeApp')
 
         //TODO:  Hygiene here for max length of inputs.
         this.signup = function(info, callback) {
-        	var username=info.username;
-        	var password=info.password;
-        	var email=info.email;
+            var username = info.username;
+            var password = info.password;
+            var email = info.email;
 
 
-        	console.log("signup", info);
+            console.log("signup", info);
 
             // verify info for all the elements in api
 
-                $http.post('api/v1/register', {
-                        username: username,
-                        password: password,
-                        email: email
-                });
+            $http.post('api/v1/register', {
+                username: username,
+                password: password,
+                email: email
+            }).success(function(data) {
+                console.log("registration successful");
+                callback(null);
+            }).error(function(data) {
+                //callback(data);
+                console.log("error", data);
+                callback('Invalid Login and/or Password.');
+            });
+
+            /*
                 $http.post('api/v1/record/demographics', {
                     "demographics.name.first": info.firstName = '',
                     "demographics.name.middle": info.middleName = '',
@@ -35,21 +44,8 @@ angular.module('phrPrototypeApp')
                     "demographics.dob": info.dob = '',
                     "demographics.gender": info.gender = ''
                 })
-                    .success(function(data) {
-			        	console.log("registration successful");
-                        callback(null);
-                    }).error(function(data) {
-                        //callback(data);
-			        	console.log("error", data);
-                        callback('Invalid Login and/or Password.');
-                    });
+*/
 
-                //Stubbed login.
-                /*(if ((username === 'test' && password === 'test') || (username === 'test@amida-demo.com' && password === 'test')) {
-                    callback(null);
-                } else {
-                    callback('Invalid Login and/or Password.');
-                }*/
-            //}
+
         };
     });
