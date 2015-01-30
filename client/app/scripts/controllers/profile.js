5aa5e01a459e1fe311ebca89729cc2833bcee3fd
Ekavali Mishra <ekavali@amida-tech.com>
2015-01-30 12:23:54 -0500

    dob working, edit address, phone

--- client/app/scripts/controllers/profile.js
+++ client/app/scripts/controllers/profile.js
@@ -33,15 +33,19 @@ angular.module('phrPrototypeApp')
         $scope.profile = profileInfo;
         // console.log('profile controller', $scope.profile._id);
         //Shims for HL7 weirdness.
-        if (profileInfo.dob.date) {
-          var tmpDOB = moment(profileInfo.dob.date).format('YYYY-MM-DD');
-          $scope.profile.dob = tmpDOB;
-        } else {
+        
+        var tmpDOB = moment(profileInfo.dob.date).format('YYYY-MM-DD');
+        $scope.profile.dob = tmpDOB;
         $scope.user_first = profileInfo.name.first;
         $scope.user_last = profileInfo.name.last;
         $scope.user_email = profileInfo.email[0].email;
         $scope.user_dob = profileInfo.dob;
-      }
+
+        // hard coded language
+        if ($scope.profile.languages[0].language === 'en') {
+          $scope.user_language = 'English';
+        }
+
       });
     }
 
@@ -51,29 +55,3 @@ angular.module('phrPrototypeApp')
     });
 
 
-          // 'email': $scope.profile.email[0].email,
-          // 'firstName': $scope.profile.name.first,
-          // 'middleName': $scope.profile.name.middle[0],
-          // 'lastName': $scope.profile.name.last,
-          // 'dob': $scope.profile.dob,
-          // 'gender': $scope.profile.gender,
-          // 'race_ethnicity': $scope.profile.race_ethnicity,
-          // 'marital_status': $scope.profile.marital_status,
-          // 'religion': $scope.profile.religion,
-          // 'db_id': $scope.profile._id,
-          // 'record_id': $scope.profile.metadata.attribution[0].record._id
-
-
-      // var info = {
-      //   "name": {
-      //       "middle": [$scope.inputMiddle],
-      //       "last": $scope.inputLast,
-      //       "first": $scope.inputFirst
-      //   },
-      //   "dob": $scope.inputDOB,
-      //   "gender": $scope.inputGender,
-      //   "email": [{
-      //       "email": $scope.inputEmail,
-      //       "type": "primary"
-      //   }]
-      // };
