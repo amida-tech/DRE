2b3e48042632ba97112cddf0d20c2baeb25199f1
Ekavali Mishra <ekavali@amida-tech.com>
2015-02-02 10:37:57 -0500

    logic to check for name and email

--- client/app/scripts/directives/profile.js
+++ client/app/scripts/directives/profile.js
@@ -19,7 +19,7 @@ angular.module('phrPrototypeApp')
                 profile.getProfile(function(err, profileInfo) {
                     scope.user_first = profileInfo.name.first;
                     scope.user_last = profileInfo.name.last;
-                    //scope.user_email = profileInfo.email[0].email;
+                    scope.user_email = profileInfo.email[0].email;
                     scope.user_dob = profileInfo.dob;
                 });
 
