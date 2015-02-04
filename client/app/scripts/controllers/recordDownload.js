1aa77897eefb698aca2c68ef86eeef70e2f82f2b
Ekavali Mishra <ekavali@amida-tech.com>
2015-02-04 16:30:09 -0500

    controller

---
+++ client/app/scripts/controllers/recordDownload.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name phrPrototypeApp.controller:DownloadCtrl
+ * @description
+ * # DownloadCtrl
+ * Controller of the phrPrototypeApp
+ */
+angular.module('phrPrototypeApp')
+  .controller('RecordDownloadCtrl', function ($scope, $location) {
+
+
+
+
+
+
+  });
