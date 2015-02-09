534e655261cc0589b272de2d53253b5139a7fec1
Ekavali Mishra <ekavali@amida-tech.com>
2015-02-09 09:50:51 -0500

    partial commit
    redirects to login even when it shouldn’t

--- client/app/scripts/app.js
+++ client/app/scripts/app.js
@@ -9,97 +9,120 @@
  * Main module of the application.
  */
 angular
-  .module('phrPrototypeApp', [
-    'ngAnimate',
-    'ngCookies',
-    'ngResource',
-    'ngRoute',
-    'ngSanitize',
-    'ngTouch',
-    'mgcrea.ngStrap',
-    'd3'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl'
-      })
-      .when('/login', {
-        templateUrl: 'views/login.html',
-        controller: 'LoginCtrl'
-      })
-      .when('/home', {
-        templateUrl: 'views/home.html',
-        controller: 'HomeCtrl'
-      })
-      .when('/register', {
-        templateUrl: 'views/register.html',
-        controller: 'RegisterCtrl'
-      })
-      .when('/profile', {
-        templateUrl: 'views/profile.html',
-        controller: 'ProfileCtrl'
-      })
-      .when('/account', {
-        templateUrl: 'views/account.html',
-        controller: 'AccountCtrl'
-      })
-      .when('/reset', {
-        templateUrl: 'views/reset.html',
-        controller: 'ResetCtrl'
-      })
-      .when('/account', {
-        templateUrl: 'views/account.html',
-        controller: 'AccountCtrl'
-      })
-      .when('/files', {
-        templateUrl: 'views/files.html',
-        controller: 'FilesCtrl'
-      })
-      .when('/record', {
-        templateUrl: 'views/record.html',
-        controller: 'RecordCtrl'
-      })
-      .when('/billing/claims', {
-        templateUrl: 'views/billing/claims.html',
-        controller: 'BillingClaimsCtrl'
-      })
-      .when('/billing/insurance', {
-        templateUrl: 'views/billing/insurance.html',
-        controller: 'BillingInsuranceCtrl'
-      })
-      .when('/billing', {
-        templateUrl: 'views/billing.html',
-        controller: 'BillingCtrl'
-      })
-      .when('/review', {
-        templateUrl: 'views/review.html',
-        controller: 'ReviewCtrl'
-      })
-      .when('/record/download', {
-        templateUrl: 'views/record/download.html',
-        controller: 'RecordDownloadCtrl'
-      })
-      .when('/files/upload', {
-        templateUrl: 'views/files/upload.html',
-        controller: 'FilesUploadCtrl'
-      })
-      .when('/notes', {
-        templateUrl: 'views/notes.html',
-        controller: 'NotesCtrl'
-      })
-      .when('/matches', {
-        templateUrl: 'views/matches.html',
-        controller: 'MatchesCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+    .module('phrPrototypeApp', [
+        'ngAnimate',
+        'ngCookies',
+        'ngResource',
+        'ngRoute',
+        'ngSanitize',
+        'ngTouch',
+        'mgcrea.ngStrap',
+        'd3'
+    ])
+    .config(function($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'views/main.html',
+                controller: 'MainCtrl'
+            })
+            .when('/about', {
+                templateUrl: 'views/about.html',
+                controller: 'AboutCtrl'
+            })
+            .when('/login', {
+                templateUrl: 'views/login.html',
+                controller: 'LoginCtrl'
+            })
+            .when('/home', {
+                templateUrl: 'views/home.html',
+                controller: 'HomeCtrl'
+            })
+            .when('/register', {
+                templateUrl: 'views/register.html',
+                controller: 'RegisterCtrl'
+            })
+            .when('/profile', {
+                templateUrl: 'views/profile.html',
+                controller: 'ProfileCtrl'
+            })
+            .when('/account', {
+                templateUrl: 'views/account.html',
+                controller: 'AccountCtrl'
+            })
+            .when('/reset', {
+                templateUrl: 'views/reset.html',
+                controller: 'ResetCtrl'
+            })
+            .when('/account', {
+                templateUrl: 'views/account.html',
+                controller: 'AccountCtrl'
+            })
+            .when('/files', {
+                templateUrl: 'views/files.html',
+                controller: 'FilesCtrl'
+            })
+            .when('/record', {
+                templateUrl: 'views/record.html',
+                controller: 'RecordCtrl'
+            })
+            .when('/billing/claims', {
+                templateUrl: 'views/billing/claims.html',
+                controller: 'BillingClaimsCtrl'
+            })
+            .when('/billing/insurance', {
+                templateUrl: 'views/billing/insurance.html',
+                controller: 'BillingInsuranceCtrl'
+            })
+            .when('/billing', {
+                templateUrl: 'views/billing.html',
+                controller: 'BillingCtrl'
+            })
+            .when('/review', {
+                templateUrl: 'views/review.html',
+                controller: 'ReviewCtrl'
+            })
+            .when('/record/download', {
+                templateUrl: 'views/record/download.html',
+                controller: 'RecordDownloadCtrl'
+            })
+            .when('/files/upload', {
+                templateUrl: 'views/files/upload.html',
+                controller: 'FilesUploadCtrl'
+            })
+            .when('/notes', {
+                templateUrl: 'views/notes.html',
+                controller: 'NotesCtrl'
+            })
+            .when('/matches', {
+                templateUrl: 'views/matches.html',
+                controller: 'MatchesCtrl'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    })
+    .run(function($rootScope, $location, authentication) {
 
+        var routesThatDontRequireAuth = ['/login', '/', '/register'];
 
+        check if current location matches route  
+        var routeClean = function (route) {
+          return _.find(routesThatDontRequireAuth)
+        };
+
+        $rootScope.$on('$routeChangeStart', function(event, next, current) {
+            // if route requires auth and user is not logged in
+            authentication.authStatus(function(err, res) {
+                if (!res) {
+                    var url = $location.url();
+                    console.log('url', url==='/login');
+                    if (url === '/') {
+                        console.log('no login required');
+                    } else {
+                        $location.path('/login');
+                        console.log('whyyy');
+                    }
+                }
+            });
+        });
+    });
