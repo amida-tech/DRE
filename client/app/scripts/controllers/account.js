807cf0ac5cf7dc0828a175dab6a9a82286ca321b
Dmitry Kachaev <dmitry.kachaev@gmail.com>
2015-02-10 01:34:37 -0500

    password change is working
    need ui cleanup (e.g. error handling)

--- client/app/scripts/controllers/account.js
+++ client/app/scripts/controllers/account.js
@@ -8,17 +8,35 @@
  * Controller of the phrPrototypeApp
  */
 angular.module('phrPrototypeApp')
-  .controller('AccountCtrl', function ($scope, $location) {
-    
-    $scope.resetPassword = function () {
-    	if ($scope.inputNewPassword === $scope.inputRepeatPassword) {
-    		$scope.resetForm.$setPristine();
-    		$scope.error = null;
-    	} else {
-    		$scope.error = "New passwords did not match";
-    	}
+    .controller('AccountCtrl', function($scope, $location, $http) {
 
-    };
+        $scope.resetPassword = function() {
+            if ($scope.inputNewPassword === $scope.inputRepeatPassword) {
+                $scope.resetForm.$setPristine();
+                $scope.error = null;
+
+                console.log("password changed here");
+
+                var info = {
+                    "old": $scope.inputOldPassword,
+                    "new": $scope.inputNewPassword
+                };
+
+                $http.post('api/v1/changepassword', info)
+                    .success(function(data) {
+                        console.log("password change successful");
+                        $location.path('/home');
+                    }).error(function(data) {
+                        console.log('password change failed');
+                        $location.path('/home');
+                    });
 
 
-  });
+            } else {
+                $scope.error = "New passwords did not match";
+            }
+
+        };
+
+
+    });
