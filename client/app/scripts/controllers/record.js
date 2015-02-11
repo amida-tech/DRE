686fb946ef04e7945ca3bd592e941d3502b5d4d9
Dmitry Kachaev <dmitry.kachaev@gmail.com>
2015-02-11 01:11:20 -0500

    data service is working for records

--- client/app/scripts/controllers/record.js
+++ client/app/scripts/controllers/record.js
@@ -6,11 +6,40 @@
  * # RecordCtrl
  * Controller of the phrPrototypeApp
  */
-angular.module('phrPrototypeApp').controller('RecordCtrl', function($scope, $window, $location, record, format, matches, merges, history) {
+angular.module('phrPrototypeApp').controller('RecordCtrl', function($scope, $window, $location, record, format, matches, merges, history, dataservice) {
     console.log("RECORD CONTROLLER LOAD ");
-    angular.element("#nav" + $scope.entryType).removeClass("active");
+    //angular.element("#nav" + $scope.entryType).removeClass("active");
     $scope.entryType = "all";
-    angular.element("#nav" + $scope.entryType).addClass("active");
+    //angular.element("#nav" + $scope.entryType).addClass("active");
+
+
+    console.log(Date.now(), " MAGIC OF DATASERVICE STARTS!");
+
+
+    //TODO may need callback
+    function refresh() {
+        dataservice.curr_section = $scope.entryType;
+        dataservice.getData(function() {
+            console.log(Date.now(), "MAGIC IS HERE: ", dataservice.processed_record);
+            //console.log("MORE: ", dataservice.all_merges, dataservice.merges_record, dataservice.merges_billing);
+
+
+            //update merges in scope
+            $scope.mergesList_record = dataservice.merges_record;
+            $scope.mergesList_billing = dataservice.merges_billing;
+            $scope.mergesList = dataservice.all_merges;
+
+
+            pageRender(dataservice.master_record, dataservice.all_notes);
+            $scope.masterMatches = dataservice.curr_processed_matches;
+
+        });
+    };
+
+    refresh();
+
+
+
     //Flip All as active selected item in DOM
     function getHistory() {
         history.getHistory(function(err, history) {
@@ -23,33 +52,9 @@ angular.module('phrPrototypeApp').controller('RecordCtrl', function($scope, $win
         });
     }
     getHistory();
-    //Loading Merges
-    merges.getMerges(function(err, data) {
-        if (err) {
-            console.log("error whil getting merges ", err);
-        } else {
-            var filtered_merges=[];
-            var filtered_record=[];
-            var filtered_billing=[];
-            
-            _.each(data, function(merge){
-                //console.log(merge);
 
-                // no claims and payers merges here
-                if (!_.contains(['claims',  'payers'], merge.entry_type)) {
-                    filtered_record.push(merge);
-                } else {
-                    filtered_billing.push(merge);
-                }
-            });
-            
-            $scope.mergesList_record = filtered_record;
-            $scope.mergesList_billing = filtered_billing;
 
-            $scope.mergesList = data;
-            //console.log("merges data ", $scope.mergesList);
-        }
-    });
+
     // produces singular name for section name - in records merges list
     $scope.singularName = function(section) {
         switch (section) {
@@ -100,19 +105,22 @@ angular.module('phrPrototypeApp').controller('RecordCtrl', function($scope, $win
             }
         });
         $scope.pageLoaded = false;
+
+        /*
         if (_.isEmpty(matches.getSection())) {
             $scope.entryType = "all";
         } else {
             $scope.entryType = matches.getSection();
         }
+    */
+
+
         //Flip All as active selected item in DOM
         angular.element("#nav" + $scope.entryType).addClass("active");
 
-        if (_.isEmpty(record.processedRecord)) {
-            $scope.recordEntries = record.processRecord(data, data_notes, "record.js controller");
-        } else {
-            $scope.recordEntries = record.processedRecord;
-        }
+
+        $scope.recordEntries = dataservice.processed_record;
+
 
         $scope.recordEntries = _.sortBy($scope.recordEntries, function(entry) {
             if (entry.metadata.datetime[0]) {
@@ -121,6 +129,7 @@ angular.module('phrPrototypeApp').controller('RecordCtrl', function($scope, $win
                 return '1979-12-12';
             }
         }).reverse();
+
         if ($scope.entryType === "all") {
             $scope.entryListFiltered = $scope.recordEntries;
         } else {
@@ -128,89 +137,47 @@ angular.module('phrPrototypeApp').controller('RecordCtrl', function($scope, $win
                 category: $scope.entryType
             });
         }
+
         $scope.$watch('entryType', function(newVal, oldVal) {
             //keeping current section name in scope
             $scope.entryType = newVal;
             console.log("$scope.entryType = ", $scope.entryType);
-            getMatchesData();
-            if (newVal !== oldVal) {
-                if (newVal === "all") {
-                    $scope.entryListFiltered = $scope.recordEntries;
-                    matches.setSection("");
-                } else {
-                    $scope.entryListFiltered = _.where($scope.recordEntries, {
-                        category: newVal
-                    });
-                    matches.setSection(newVal);
+
+
+            //TODO get matches data again, here
+
+            dataservice.curr_section = $scope.entryType;
+            dataservice.getMatchesData(function() {
+
+                $scope.masterMatches=dataservice.curr_processed_matches;
+                $scope.recordEntries=dataservice.processed_record;
+
+                if (newVal !== oldVal) {
+                    if (newVal === "all") {
+                        $scope.entryListFiltered = $scope.recordEntries;
+                        
+                        dataservice.curr_section="";
+                    } else {
+                        $scope.entryListFiltered = _.where($scope.recordEntries, {
+                            category: newVal
+                        });
+                        dataservice.curr_section=newVal;
+                    }
+                    if (newVal === "vitals") {
+                        $scope.$broadcast('tabchange', {
+                            "val": 0
+                        });
+                    }
                 }
-                if (newVal === "vitals") {
-                    $scope.$broadcast('tabchange', {
-                        "val": 0
-                    });
-                }
-            }
+            });
+
+
+
         });
     }
 
     //console.log(">>>>>>", record.masterRecord, record.recordDirty);
 
-    if (_.isEmpty(record.masterRecord) || record.recordDirty) {
-        console.log("MASTER DATA IS EMPTY OR DIRTY");
-        record.getData(function(err, data) {
-            //getNotes and associate them with record
-            record.setNotes(data.notes);
-            record.setMasterRecord(data.records);
-            pageRender(data.records, data.notes);
-        });
-    } else {
-        pageRender(record.masterRecord, record.all_notes);
-    }
-    $scope.masterMatches = {};
-
-
-    // Get Matches data for partial matches 
-    function getMatchesData() {
-        //console.log("getting merges for section ", $scope.entryType);
-        if (!$scope.entryType || $scope.entryType === 'all') {
-            return;
-        }
-        matches.getCategory($scope.entryType).then(function(data) {
-            $scope.masterMatches = {
-                'category': $scope.entryType,
-                'data': data.matches,
-                'count': data.matches.length
-            };
-            //Wire matches into the record
-            _.each($scope.masterMatches.data, function(match) {
-                var match_count = 0;
-                //console.log(mat)
-                //console.log("match id and master entry id", match._id, match.entry._id, $scope.recordEntries);
-                //find $scope.recordEntries.data._id === match.entry._id
-                _.each($scope.recordEntries, function(recordEntry) {
-                    if (recordEntry.data._id === match.matches[0].match_entry._id) {
-                        //console.log("attaching match ", recordEntry, match);
-
-                        //calculate number of pending matches per entry
-
-                        if (recordEntry.metadata.match && recordEntry.metadata.match.count) {
-                            match_count = recordEntry.metadata.match.count + 1;
-                        } else {
-                            match_count = 1;
-                        }
-                        recordEntry.metadata.match = {
-                            'match_id': match._id,
-                            'section': $scope.entryType,
-                            'count': match_count
-                        };
-                    }
-                });
-            });
-            //do stuff here
-            $scope.allergyMatch = $scope.masterMatches.data[1];
-        });
-    }
-
-    getMatchesData();
 
     $scope.goToMatches = function(section) {
         //console.log(section);
