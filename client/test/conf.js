2ec41df69b9372052e9bcbff319474e34bf89b98
Jamie Johnson <jamiesanerd@gmail.com>
2015-02-19 11:49:36 -0500

    upload file

--- client/test/conf.js
+++ client/test/conf.js
@@ -2,7 +2,7 @@
 exports.config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
   suites: {
-    register: ['register.js','logout.js'],
-    login: ['login.js','logout.js']
+    register: ['register.js','upload.js','logout.js'],
+    login: ['login.js','upload.js','logout.js']
   },
 }
