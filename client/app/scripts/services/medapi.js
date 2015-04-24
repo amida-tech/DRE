0c98e5055f594e22fad9753617422f3d9968c88c
Ekavali Mishra <ekavali@amida-tech.com>
2015-04-24 18:06:59 -0400

    add medapi service
    partial commit for integrating API with UI

---
+++ client/app/scripts/services/medapi.js
@@ -0,0 +1,22 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name phrPrototypeApp.openfda
+ * @description
+ * # openfda
+ * Service in the phrPrototypeApp.
+ */
+angular.module('phrPrototypeApp').service('openfda', function openfda($http) {
+	
+	this.getFDAdata = function(rxNormCode, callback) {
+		$http.get('api/v1/openfda')
+			.success(function (data) {
+				callback(null, data)
+			}).error(function (err) {
+				callback(err);
+			});
+
+	};
+
+)};
