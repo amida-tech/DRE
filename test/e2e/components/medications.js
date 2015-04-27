9e78af83878287d19834549d1d9f85c80ddee531
OEHokie <matt.martz@gmail.com>
2015-04-27 15:09:55 -0400

    merging 1.5 into this

--- test/e2e/components/medications.js
+++ test/e2e/components/medications.js
@@ -1,34 +1,11 @@
 var expect = require('chai').expect;
 var supertest = require('supertest');
 var deploymentLocation = 'http://' + 'localhost' + ':' + '3000';
-var databaseLocation = 'mongodb://' + 'localhost' + '/' + process.env.DBname || 'tests';
 var api = supertest.agent(deploymentLocation);
 var fs = require('fs');
 var path = require('path');
-var database = require('mongodb').Db;
+var common = require('../../common/common.js');
 var common2 = require('../common.js');
-
-function removeCollection(inputCollection, callback) {
-    var db;
-    database.connect(databaseLocation, function (err, dbase) {
-        if (err) {
-            throw err;
-        }
-        db = dbase;
-        db.collection(inputCollection, function (err, coll) {
-            if (err) {
-                throw err;
-            }
-            coll.remove({}, function (err, results) {
-                if (err) {
-                    throw err;
-                }
-                db.close();
-                callback();
-            });
-        });
-    });
-}
 
 function loadTestRecord(fileName, callback) {
     var filepath = path.join(__dirname, '../../artifacts/test-r1.0/' + fileName);
@@ -46,23 +23,23 @@ function loadTestRecord(fileName, callback) {
 describe('Pre Test Cleanup', function () {
 
     it('Remove Medication Collections', function (done) {
-        removeCollection('medications', function (err) {
+        common.removeCollection('medications', function (err) {
             if (err) {
                 done(err);
             }
-            removeCollection('medicationsmerges', function (err) {
+            common.removeCollection('medicationsmerges', function (err) {
                 if (err) {
                     done(err);
                 }
-                removeCollection('medicationsmatches', function (err) {
+                common.removeCollection('medicationsmatches', function (err) {
                     if (err) {
                         done(err);
                     }
-                    removeCollection('storage.files', function (err) {
+                    common.removeCollection('storage.files', function (err) {
                         if (err) {
                             done(err);
                         }
-                        removeCollection('storage.chunks', function (err) {
+                        common.removeCollection('storage.chunks', function (err) {
                             if (err) {
                                 done(err);
                             }
