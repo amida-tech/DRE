8ba0c27adcbd4acc8c0d778468d9a60e12640ecc
OEHokie <matt.martz@gmail.com>
2015-04-28 16:27:53 -0400

    typo

--- client/app/scripts/services/medapi.js
+++ client/app/scripts/services/medapi.js
@@ -12,11 +12,11 @@ angular.module('phrPrototypeApp').service('openfda', function openfda($http) {
 	this.getFDAdata = function(rxNormCode, callback) {
 		$http.get('api/v1/openfda')
 			.success(function (data) {
-				callback(null, data)
+				callback(null, data);
 			}).error(function (err) {
 				callback(err);
 			});
 
 	};
 
-)};
+});
