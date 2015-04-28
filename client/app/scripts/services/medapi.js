46a14b9ba8a85e74df12daa143a8a7cdd24ab9ee
OEHokie <matt.martz@gmail.com>
2015-04-28 16:28:30 -0400

    ?

--- client/app/scripts/services/medapi.js
+++ client/app/scripts/services/medapi.js
@@ -8,15 +8,15 @@
  * Service in the phrPrototypeApp.
  */
 angular.module('phrPrototypeApp').service('openfda', function openfda($http) {
-	
-	this.getFDAdata = function(rxNormCode, callback) {
-		$http.get('api/v1/openfda')
-			.success(function (data) {
-				callback(null, data);
-			}).error(function (err) {
-				callback(err);
-			});
 
-	};
+    this.getFDAdata = function (rxNormCode, callback) {
+        $http.get('api/v1/openfda')
+            .success(function (data) {
+                callback(null, data);
+            }).error(function (err) {
+                callback(err);
+            });
+
+    };
 
 });
