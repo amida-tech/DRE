44392e64f2b7b3667d91109b042b2150963caeac
Jacob Sachs <jacob.s.sachs@gmail.com>
2015-05-03 11:27:40 -0500

    timeout should fix the issue

--- client/app/scripts/directives/focus.js
+++ client/app/scripts/directives/focus.js
@@ -10,10 +10,9 @@ angular.module('phrPrototypeApp')
                 link: function (scope, element) {
                     scope.$watch('trigger', function (value) {
                         if (value === true) {
-                            // $timeout(function () {
+                            $timeout(function () {
                                 element[0].focus();
-                                // scope.trigger = false;
-                            // });
+                            });
                         }
                     });
                 }
