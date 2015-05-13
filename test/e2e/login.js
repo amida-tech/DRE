4d672b0af1ba3ac4f0a2745a2172de7421498750
OEHokie <matt.martz@gmail.com>
2015-05-13 10:16:41 -0400

    linting

--- test/e2e/login.js
+++ test/e2e/login.js
@@ -9,9 +9,9 @@ describe('Array', function () {
         xit('should return -1 when the value is not present', function () {
             assert.equal(-1, [1, 2, 3].indexOf(5));
             assert.equal(-1, [1, 2, 3].indexOf(0));
-        })
-    })
-})
+        });
+    });
+});
 
 describe('authentication', function (done) {
     it('should be unauthenticated', function (done) {
@@ -19,11 +19,13 @@ describe('authentication', function (done) {
             .get('/api/v1/account')
             .expect(200)
             .end(function (err, res) {
-                if (err) throw err;
+                if (err) {
+                    throw err;
+                }
                 expect(res.body.authenticated).to.equal(false);
                 done();
             });
-    })
+    });
 
     it('should register', function (done) {
         api
@@ -46,7 +48,7 @@ describe('authentication', function (done) {
                 if (err) throw err;
                 done();
             });
-    })
+    });
 
     it('should still be unauthenticated', function (done) {
         api
@@ -57,11 +59,11 @@ describe('authentication', function (done) {
                 expect(res.body.authenticated).to.equal(false);
                 done();
             });
-    })
+    });
 
     it('should login', function (done) {
         common.login(api, 'test', 'test', done);
-    })
+    });
 
     it('should be authenticated', function (done) {
         api
@@ -72,7 +74,7 @@ describe('authentication', function (done) {
                 expect(res.body.authenticated).to.equal(true);
                 done();
             });
-    })
+    });
 
     it('should logout', function (done) {
         api
@@ -82,7 +84,7 @@ describe('authentication', function (done) {
                 if (err) throw err;
                 done();
             });
-    })
+    });
 
     it('should be unauthenticated again', function (done) {
         api
@@ -93,5 +95,5 @@ describe('authentication', function (done) {
                 expect(res.body.authenticated).to.equal(false);
                 done();
             });
-    })
-})
+    });
+});
