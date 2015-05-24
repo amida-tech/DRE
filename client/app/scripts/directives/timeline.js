6848981e7fc62f187c8853744eb8e1e65af25181
Jacob Sachs <jacob.s.sachs@gmail.com>
2015-05-24 00:04:16 -0500

    fix function declaration lint errors

--- client/app/scripts/directives/timeline.js
+++ client/app/scripts/directives/timeline.js
@@ -118,164 +118,165 @@ angular.module('phrPrototypeApp').directive('timeline', function($window, $locat
             }
 
             function renderPlot() {
-                    if (dataToPlot.length > 0) {
-                        console.log("timeline render plot");
+                var width = 0;
+                if (dataToPlot.length > 0) {
+                    console.log("timeline render plot");
+                    getSVGWidth();
+                    buildScale();
+                    buildTicks();
+                    structureData();
+                    plotData();
+                }
+                
+                function getSVGWidth() {
+                    width = parseInt(svg.style('width'), 10);
+                    console.log("svg width = " + width);
+                    console.log("element width = " + element.width());
 
-                        var width = 0;
+                    //Shim, keeps it from erroring on first pass.
+                    if (width === 0) {
+                        width = $window.innerWidth * 0.67;
+                    }
 
-                        function getSVGWidth() {
-                            width = parseInt(svg.style('width'), 10);
-                            console.log("svg width = " + width);
-                            console.log("element width = " + element.width());
+                }
+                
+                function buildScale() {
+                    timeScale = d3.time.scale().range([(boundaryOffset + (boundaryWidth / 2)), width - (boundaryOffset - (boundaryWidth / 2))]).domain(plotDomain);
+                }
 
-                            //Shim, keeps it from erroring on first pass.
-                            if (width === 0) {
-                                width = $window.innerWidth * 0.67;
-                            }
-
-                        }
-
-                        function buildScale() {
-                            timeScale = d3.time.scale().range([(boundaryOffset + (boundaryWidth / 2)), width - (boundaryOffset - (boundaryWidth / 2))]).domain(plotDomain);
-                        }
-
-                        function buildTicks() {
-                            timeScaleTicks = [];
-                            //TODO:  Configure this for monthly ticks.
-                            var tmpTimeScaleTicks = timeScale.ticks(20);
-                            for (var i in tmpTimeScaleTicks) {
-                                timeScaleTicks.push({
-                                    "x_axis": timeScale(tmpTimeScaleTicks[i]),
-                                    "y_axis": (plotHeight - boundaryLabelOffset - boundaryLabelPadding) / 2
-                                });
-                            }
-                        }
-
-                        function structureData() {
-                            for (var i in plotCircles) {
-                                plotCircles[i].x_axis = timeScale(plotCircles[i].date);
-                                plotCircles[i].y_axis = (plotHeight - boundaryLabelOffset - boundaryLabelPadding) / 2;
-                                plotCircles[i].radius = 10;
-                                plotCircles[i].color = plotBaseColor;
-                                plotCircles[i].href = "entry" + i;
-                            }
-                        }
-
-                        function plotData() {
-                            svg.selectAll('*').remove();
-                            var boundaryData = [{
-                                "x": boundaryOffset,
-                                "y": 0,
-                                "width": boundaryWidth,
-                                "height": plotHeight - boundaryLabelOffset - boundaryLabelPadding,
-                                "color": plotBaseColor
-                            }, {
-                                "x": width - boundaryOffset,
-                                "y": 0,
-                                "width": boundaryWidth,
-                                "height": plotHeight - boundaryLabelOffset - boundaryLabelPadding,
-                                "color": plotBaseColor
-                            }];
-                            var plotLineData = [{
-                                "x": boundaryOffset,
-                                "y": ((plotHeight - boundaryLabelOffset - boundaryLabelPadding) / 2) - 1,
-                                "width": width - boundaryOffset - boundaryOffset,
-                                "height": 3,
-                                "color": plotBaseColor
-                            }];
-                            var boundaryDisplayFormat = d3.time.format("%b %Y");
-                            var boundaryLabel = [{
-                                "x": 0,
-                                "y": plotHeight - boundaryLabelPadding,
-                                "anchor": "start",
-                                "text": boundaryDisplayFormat(plotDomain[0])
-                            }, {
-                                "x": width,
-                                "y": plotHeight - boundaryLabelPadding,
-                                "anchor": "end",
-                                "text": boundaryDisplayFormat(plotDomain[1])
-                            }];
-                            var boundaryLabels = svg.selectAll("text").data(boundaryLabel).enter().append("text");
-                            var boundaryLabelAttributes = boundaryLabels.attr("x", function(d) {
-                                return d.x;
-                            }).attr("y", function(d) {
-                                return d.y;
-                            }).text(function(d) {
-                                if (d.text === " 0NaN") { // Used for an empty timeline
-                                    return " ";
-                                } else {
-                                    return d.text;
-                                }
-                            }).style("text-anchor", function(d) {
-                                return d.anchor;
-                            });
-                            var boundaries = svg.selectAll("plotBoundary").data(boundaryData).enter().append("rect");
-                            var boundaryAttributes = boundaries.attr("x", function(d) {
-                                return d.x;
-                            }).attr("y", function(d) {
-                                return d.y;
-                            }).attr("width", function(d) {
-                                return d.width;
-                            }).attr("height", function(d) {
-                                return d.height;
-                            }).style("fill", function(d) {
-                                return d.color;
-                            });
-                            var plotLine = svg.selectAll("plotLine").data(plotLineData).enter().append("rect");
-                            var plotLineAttributes = plotLine.attr("x", function(d) {
-                                return d.x;
-                            }).attr("y", function(d) {
-                                return d.y;
-                            }).attr("width", function(d) {
-                                return d.width;
-                            }).attr("height", function(d) {
-                                return d.height;
-                            }).style("fill", function(d) {
-                                return d.color;
-                            });
-                            /*var plotLines = svg.selectAll("plotLines").data(timeScaleTicks).enter().append("circle");
-                            var plotLineAttributes = plotLines
-                                .attr("cx", function (d) {
-                                    //console.log(d);
-                                    return d.x_axis;
-                                })
-                                .attr("cy", function (d) {
-                                    return d.y_axis;
-                                })
-                                .attr("r", 2)
-                                .style("fill", "#5bc0de")
-                                .attr("class", "plotLines");*/
-                            var circles = svg.selectAll("plotPoint").data(plotCircles).enter().append("circle");
-                            var circleAttributes = circles.attr("cx", function(d) {
-                                return d.x_axis;
-                            }).attr("cy", function(d) {
-                                return d.y_axis;
-                            }).attr("r", function(d) {
-                                return d.radius;
-                            }).attr("class", "plotPoint").on('mouseover', function(d) {
-                                var tipFormat = d3.time.format("%m/%d/%Y");
-                                if (attrs.chartLocation === 'all') {
-                                    tip.attr('class', 'd3-tip animate').html(function(d) {
-                                        //return '<span>' + d.category + '</span> - <span>' + tipFormat(d.date) + '</span>';
-                                        return '<span>' + tipFormat(d.date) + '</span>';
-                                    }).show(d);
-                                } else {
-                                    tip.attr('class', 'd3-tip animate').html(function(d) {
-                                        return '<span>' + tipFormat(d.date) + '</span>';
-                                    }).show(d);
-                                }
-                            }).on('mouseout', function(d) {
-                                tip.attr('class', 'd3-tip').show(d);
-                                tip.hide();
-                            });
-                        }
-                        getSVGWidth();
-                        buildScale();
-                        buildTicks();
-                        structureData();
-                        plotData();
+                function buildTicks() {
+                    timeScaleTicks = [];
+                    //TODO:  Configure this for monthly ticks.
+                    var tmpTimeScaleTicks = timeScale.ticks(20);
+                    for (var i in tmpTimeScaleTicks) {
+                        timeScaleTicks.push({
+                            "x_axis": timeScale(tmpTimeScaleTicks[i]),
+                            "y_axis": (plotHeight - boundaryLabelOffset - boundaryLabelPadding) / 2
+                        });
                     }
                 }
+
+                function structureData() {
+                    for (var i in plotCircles) {
+                        plotCircles[i].x_axis = timeScale(plotCircles[i].date);
+                        plotCircles[i].y_axis = (plotHeight - boundaryLabelOffset - boundaryLabelPadding) / 2;
+                        plotCircles[i].radius = 10;
+                        plotCircles[i].color = plotBaseColor;
+                        plotCircles[i].href = "entry" + i;
+                    }
+                }
+
+                function plotData() {
+                    svg.selectAll('*').remove();
+                    var boundaryData = [{
+                        "x": boundaryOffset,
+                        "y": 0,
+                        "width": boundaryWidth,
+                        "height": plotHeight - boundaryLabelOffset - boundaryLabelPadding,
+                        "color": plotBaseColor
+                    }, {
+                            "x": width - boundaryOffset,
+                            "y": 0,
+                            "width": boundaryWidth,
+                            "height": plotHeight - boundaryLabelOffset - boundaryLabelPadding,
+                            "color": plotBaseColor
+                        }];
+                    var plotLineData = [{
+                        "x": boundaryOffset,
+                        "y": ((plotHeight - boundaryLabelOffset - boundaryLabelPadding) / 2) - 1,
+                        "width": width - boundaryOffset - boundaryOffset,
+                        "height": 3,
+                        "color": plotBaseColor
+                    }];
+                    var boundaryDisplayFormat = d3.time.format("%b %Y");
+                    var boundaryLabel = [{
+                        "x": 0,
+                        "y": plotHeight - boundaryLabelPadding,
+                        "anchor": "start",
+                        "text": boundaryDisplayFormat(plotDomain[0])
+                    }, {
+                            "x": width,
+                            "y": plotHeight - boundaryLabelPadding,
+                            "anchor": "end",
+                            "text": boundaryDisplayFormat(plotDomain[1])
+                        }];
+                    var boundaryLabels = svg.selectAll("text").data(boundaryLabel).enter().append("text");
+                    var boundaryLabelAttributes = boundaryLabels.attr("x", function (d) {
+                        return d.x;
+                    }).attr("y", function (d) {
+                        return d.y;
+                    }).text(function (d) {
+                        if (d.text === " 0NaN") { // Used for an empty timeline
+                            return " ";
+                        } else {
+                            return d.text;
+                        }
+                    }).style("text-anchor", function (d) {
+                        return d.anchor;
+                    });
+                    var boundaries = svg.selectAll("plotBoundary").data(boundaryData).enter().append("rect");
+                    var boundaryAttributes = boundaries.attr("x", function (d) {
+                        return d.x;
+                    }).attr("y", function (d) {
+                        return d.y;
+                    }).attr("width", function (d) {
+                        return d.width;
+                    }).attr("height", function (d) {
+                        return d.height;
+                    }).style("fill", function (d) {
+                        return d.color;
+                    });
+                    var plotLine = svg.selectAll("plotLine").data(plotLineData).enter().append("rect");
+                    var plotLineAttributes = plotLine.attr("x", function (d) {
+                        return d.x;
+                    }).attr("y", function (d) {
+                        return d.y;
+                    }).attr("width", function (d) {
+                        return d.width;
+                    }).attr("height", function (d) {
+                        return d.height;
+                    }).style("fill", function (d) {
+                        return d.color;
+                    });
+                    /*var plotLines = svg.selectAll("plotLines").data(timeScaleTicks).enter().append("circle");
+                    var plotLineAttributes = plotLines
+                        .attr("cx", function (d) {
+                            //console.log(d);
+                            return d.x_axis;
+                        })
+                        .attr("cy", function (d) {
+                            return d.y_axis;
+                        })
+                        .attr("r", 2)
+                        .style("fill", "#5bc0de")
+                        .attr("class", "plotLines");*/
+                    var circles = svg.selectAll("plotPoint").data(plotCircles).enter().append("circle");
+                    var circleAttributes = circles.attr("cx", function (d) {
+                        return d.x_axis;
+                    }).attr("cy", function (d) {
+                        return d.y_axis;
+                    }).attr("r", function (d) {
+                        return d.radius;
+                    }).attr("class", "plotPoint").on('mouseover', function (d) {
+                        var tipFormat = d3.time.format("%m/%d/%Y");
+                        if (attrs.chartLocation === 'all') {
+                            tip.attr('class', 'd3-tip animate').html(function (d) {
+                                //return '<span>' + d.category + '</span> - <span>' + tipFormat(d.date) + '</span>';
+                                return '<span>' + tipFormat(d.date) + '</span>';
+                            }).show(d);
+                        } else {
+                            tip.attr('class', 'd3-tip animate').html(function (d) {
+                                return '<span>' + tipFormat(d.date) + '</span>';
+                            }).show(d);
+                        }
+                    }).on('mouseout', function (d) {
+                        tip.attr('class', 'd3-tip').show(d);
+                        tip.hide();
+                    });
+                }
+
+
+            }
                 //gatherData only on first run.
             $window.onload = function() {
                 console.log('onload');
