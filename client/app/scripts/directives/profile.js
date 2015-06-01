20155a5fe60b70ae20156aee7f642103011be64f
OEHokie <matt.martz@gmail.com>
2015-06-01 15:05:56 -0400

    refactor demographic calling Also fixes a bug from last PR on martz-auth

--- client/app/scripts/directives/profile.js
+++ client/app/scripts/directives/profile.js
@@ -7,28 +7,31 @@
  * # profile
  */
 angular.module('phrPrototypeApp')
-    .directive('profile', function (profile, format) {
-
+    //.directive('profile', function (profile, format) {
+    .directive('profile', function ($rootScope, format) {
         return {
             templateUrl: 'views/templates/profile.html',
             restrict: 'EA',
             link: function postLink(scope, element, attrs) {
                 //$scope.user_first="blah";
-
-                profile.getProfile(function (err, profileInfo) {
-                    if (profileInfo) {
-                        if (!angular.isUndefined(profileInfo.email)) {
-                            scope.user_email = profileInfo.email[0].email;
+                function displayProfile() {
+                    if ($rootScope.profile_info) {
+                        if (!angular.isUndefined($rootScope.profile_info.email)) {
+                            scope.user_email = $rootScope.profile_info.email[0].email;
                         }
-                        if (profileInfo.dob) {
-                            scope.user_dob = format.formatDate(profileInfo.dob.point);
+                        if ($rootScope.profile_info.dob) {
+                            scope.user_dob = format.formatDate($rootScope.profile_info.dob.point);
                         }
-                        if (profileInfo.gender) {
-                            scope.user_gender = profileInfo.gender;
+                        if ($rootScope.profile_info.gender) {
+                            scope.user_gender = $rootScope.profile_info.gender;
                         }
                     }
-                });
+                }
 
+                scope.$on('profileAvailable', function (evt, profileInfo) {
+                    displayProfile();
+                });
+                displayProfile();
             }
         };
     });
