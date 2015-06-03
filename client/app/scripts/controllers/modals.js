1ae90b32c07d6a92ba72ed393cba29dc6e457c97
OEHokie <matt.martz@gmail.com>
2015-06-03 17:39:09 -0400

    timestamp edit Closes #497  Will fail until blue-button-record PR: https://github.com/amida-tech/blue-button-record/pull/47 is merged/published Regarding T40

--- client/app/scripts/controllers/modals.js
+++ client/app/scripts/controllers/modals.js
@@ -812,6 +812,7 @@ angular.module('phrPrototypeApp')
                     console.log("err: " + err);
                 } else {
                     console.log("edited note saved: ", data);
+                    $scope.comments[0].date = data.datetime;
                 }
             });
             $scope.editflag = false;
