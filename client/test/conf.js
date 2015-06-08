66ddcfe753b2f88ec7fd420c689a22b8263f1707
Jacob Sachs <jacob.s.sachs@gmail.com>
2015-06-08 12:35:19 -0500

    new med gets appropriate screenshots

--- client/test/conf.js
+++ client/test/conf.js
@@ -22,7 +22,7 @@ exports.config = {
   suites: {
     populate: ['register.js','upload_1.js','upload_2.js','upload_3.js','logout.js'],
     scenarios: ['login.js','scenarios.js','match.js','logout.js'],
-    medications: ['login.js','med_view.js', 'med_new.js']
+    medications: ['login.js','med_view.js', 'med_new.js','logout.js']
   },
   capabilities: {
     browserName: 'firefox'
