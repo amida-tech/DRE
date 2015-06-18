0b900cf5d2a503a9272179a10a32871082fa01db
Harry Rickards <rickards@mit.edu>
2015-06-18 18:42:01 -0400

    Fix code coverage on 1.5

--- cover.sh
+++ cover.sh
@@ -4,7 +4,7 @@ mkdir -p test/coverage
 
 # run istanbul cover, save its PID so we can kill it later, and pipe
 # its output into file descriptor 3 so we can wait for the server to start
-exec 3< <(./node_modules/.bin/istanbul cover server.js --dir ./test/coverage --handle-sigint & echo $! >test/coverage/pid)
+exec 3< <(DBname=devtests ./node_modules/.bin/istanbul cover server.js --dir ./test/coverage --handle-sigint & echo $! >test/coverage/pid)
 
 # wait until we see "Server listening..." to indicate the server's started, then
 # kill sed
@@ -12,7 +12,7 @@ exec 3< <(./node_modules/.bin/istanbul cover server.js --dir ./test/coverage --h
 sed '/Server listening on port 3000$/q' <&3 ; cat <&3 &
 
 # run tests
-grunt mochaTest
+DBname=devtests grunt mochaTest
 
 # kill istanbul (--handle-sigint) ensures it generates coverage reports here
 kill -SIGINT $(<test/coverage/pid)
