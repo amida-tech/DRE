70c8d3134151b1fe767c55eae9bad4747d1f05d2
Ekavali Mishra <ekavali@amida-tech.com>
2015-06-23 11:34:37 -0400

    upload2

--- client/updated_test/upload_2.js
+++ client/updated_test/upload_2.js
@@ -4,8 +4,10 @@ describe('angularjs homepage', function() {
     var files = element(by.css('[ng-click="vm.navbarClick(\'files\')"]'));
 
     var pathToFiles = "../../test/artifacts/demo-r1.5/";
+    var continueButton = element(by.id('continue-button'));
+    var confirmButton = element(by.id('confirm-button'));
 
-    function uploadTest(un, pw) {
+    function uploadTest() {
     	files.click();
         var upload = element(by.css('[href="#/files/upload"]'));
         upload.click();
@@ -13,25 +15,32 @@ describe('angularjs homepage', function() {
         var fileToUpload = pathToFiles + "bluebutton-02-updated.xml";
         var absolutePath = path.resolve(__dirname, fileToUpload);
         $('input[type="file"]').sendKeys(absolutePath);
-
-        var continueButton = element(by.id('continue-button'));
-
+        expect(continueButton.isDisplayed()).toBeTruthy();     
+    }
+    
+    function continueUpload() {
         continueButton.click();
-
-        var confirmButton = element(by.id('confirm-button'));
-
+        expect(confirmButton.isDisplayed()).toBeTruthy();        
+    }
+    
+    function confirmUpload() {
         confirmButton.click();
-
-        var record = element(by.css('[ng-click="vm.navbarClick(\'record\')"]'));
-
-        record.click();
+        // browser.pause();
         
+        var fileList = element.all(by.repeater('file in vm.fileList')).count();
+        expect(fileList).toEqual(2);     
+    }
+    
+    function gotoRecord() {
+        var record = element(by.css('[ng-click="vm.navbarClick(\'record\')"]'));
+        record.click();        
+    }
+    
+    function recordTimeline() {
+        var timeline = element(by.css('.timeline-graph'));
+        expect(timeline.isDisplayed()).toBeTruthy;
     }
 
-    beforeEach(function() {
-        browser.get('http://localhost:3000/');
-        browser.driver.manage().window().setSize(1280, 1024);
-    });
 
     afterEach(function() {
     browser.manage().logs().get('browser').then(function(browserLog) {
@@ -47,13 +56,25 @@ describe('angularjs homepage', function() {
     });
   });
 
-    it('upload test', function() {
-        
-    	
-
+    it('upload bb02', function() {        
         uploadTest();
         //browser.pause();
-
-
     });
+    
+    it('continue bb02 upload', function() {        
+        continueUpload();
+        //browser.pause();
+    });
+    
+    it('confirm bb02 upload', function() {        
+        confirmUpload();
+        //browser.pause();
+    });   
+    
+    
+    it('go to record section after bb02 upload', function() {        
+        gotoRecord();
+        recordTimeline();
+        //browser.pause();
+    });    
 });
