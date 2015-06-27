640dcab320f39b41ba1325d874b30a359b1b3eac
Ekavali Mishra <ekavali@amida-tech.com>
2015-06-27 16:55:08 -0400

    register and login after reset

--- client/app/scripts/controllers/demo.js
+++ client/app/scripts/controllers/demo.js
@@ -11,12 +11,13 @@ angular
     .module('phrPrototypeApp')
     .controller('DemoCtrl', Demo);
 
-Demo.$inject = ['$location', '$window', 'dataservice', 'upload', 'demo'];
+Demo.$inject = ['$location', '$window', 'dataservice', 'upload', 'demo', 'registration', 'authentication'];
 
-function Demo($location, $window, dataservice, upload, demo) {
+function Demo($location, $window, dataservice, upload, demo, registration, authentication) {
     /* jshint validthis: true */
     var vm = this;
     vm.reset = reset;
+    vm.uploadFile = null;
 
     vm.demoClick = function demoClick(new_location) {
         dataservice.getLastSection(function (last_section) {
@@ -32,20 +33,49 @@ function Demo($location, $window, dataservice, upload, demo) {
     };
 
     function reset() {
-        demo.resetDemo(function (err, results) {
-            var file = "../../../../test/artifacts/demo-r1.5/bluebutton-01-original.xml";
-            console.log(file);
-            dataservice.forceRefresh();
+        // demo.resetDemo(function (err, results) {
+        //     var uploadFile = "../../../../test/artifacts/demo-r1.5/bluebutton-01-original.xml";
+        //     console.log(vm.uploadFile);
+        //     dataservice.forceRefresh();
 
-            $location.path('/login');
-            $window.location.reload();
-            // upload.uploadRecord(file, true, function(err, results) {
-            //     if (err) {
-            //         throw (err);
-            //     } else {
-            //         console.log('demo instance file uploaded', results);
-            //     }
-            // });
+        //     // $location.path('/login');
+        //     // $window.location.reload();
+        //     upload.uploadRecord(vm.uploadFile, true, function (err, results) {
+        //         if (err) {
+        //             console.log(err);
+        //         } else {
+        //             console.log('demo instance file uploaded', results);
+        //         }
+        //     });
+        // });
+
+        var info = {
+            'username': 'amida-demo',
+            'password': 'testtest',
+            'email': 'isabella@amida-demo.com',
+            'firstName': 'Isabella',
+            'middleName': 'Isa',
+            'lastName': 'Jones',
+            'dob': moment('1975-05-01').format('YYYY-MM-DD'),
+            'gender': 'female'
+        };
+
+        console.log("starting registration", info.dob);
+
+        registration.signup(info, function (err) {
+            // console.log("done");
+            // $location.path('/home');
+            if (err) {
+                console.log(err);
+            } else {
+                authentication.login(info.username, info.password, function (err) {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        $location.path('/demo');
+                    }
+                });
+            }
         });
     }
 
