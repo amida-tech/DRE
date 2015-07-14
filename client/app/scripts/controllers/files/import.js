0a658a9e9b3cc0e1b628d997d4a1084d82a2daed
Jacob Sachs <jacob.s.sachs@gmail.com>
2015-07-14 14:46:38 -0400

    add service functions

--- client/app/scripts/controllers/files/import.js
+++ client/app/scripts/controllers/files/import.js
@@ -11,69 +11,26 @@ angular
     .module('phrPrototypeApp')
     .controller('FilesImportCtrl', FilesImport);
 
-FilesImport.$inject = ['$location', '$route', 'importService', '$http', 'format', 'record', 'dataservice'];
+FilesImport.$inject = ['$location', '$window', 'importService', '$http', 'format', 'record', 'dataservice'];
 
-function FilesImport($location, $route, importService, $http, format, record, dataservice) {
+function FilesImport($location, $window, importService, $http, format, record, dataservice) {
     /* jshint validthis: true */
     var vm = this;
-    vm.importAndSave = importAndSave;
-    vm.incrementStep = incrementStep;
-    vm.myFile = null;
-    vm.new_dob = "";
-    vm.new_first = "";
-    vm.new_gender = "";
-    vm.new_last = "";
-    vm.new_middle = "";
     vm.returnToFiles = returnToFiles;
     vm.uploadStep = 0;
-
-    function incrementStep() {
-        vm.uploadStep = vm.uploadStep + 1;
-
-        var uploadFile;
-
-        if (vm.uploadStep === 1) {
-
-            uploadFile = vm.myFile;
-            console.log("extracting demographics", uploadFile);
-
-            upload.uploadRecord(uploadFile, true, function (err, results) {
-                console.log("file check ", results);
-
-                vm.new_first = results.name.first;
-                vm.new_last = results.name.last;
-                if (results.name.middle && results.name.middle[0]) {
-                    vm.new_middle = results.name.middle[0];
-                }
-                vm.new_dob = format.formatDate(results.dob.point);
-                vm.new_gender = results.gender;
-            });
-
-        } else if (vm.uploadStep === 2) {
-            uploadFile = vm.myFile;
-            console.log("uploading file", uploadFile);
-
-            upload.uploadRecord(uploadFile, false, function (err, results) {
-                vm.uploadStep = 0;
-                $location.path('/files');
-                //$route.reload();
-            });
-        }
-    }
 
     function returnToFiles() {
         $location.path('/files');
     }
 
-    function importAndSave() {
-        var uploadFile = vm.myFile;
-        console.log("uploading file", uploadFile);
-        upload.uploadRecord(uploadFile, false, function (err, results) {
-            vm.uploadStep = 0;
-            dataservice.forceRefresh();
-            $location.path('/files');
+    function oauthWithings() {
+        importService.getRequestToken(function(err, authUrl) {
+            if (err) {
+                // throw error
+                return;
+            }
+            $window.location.href = authUrl;
         });
-
     }
 
 }
--- client/app/scripts/services/files/import.js
+++ client/app/scripts/services/files/import.js
@@ -15,4 +15,25 @@ importService.$inject = ['$http', 'dataservice', 'history', 'notes'];
 
 function importService($http, dataservice, history, notes) {
     /* jshint validthis: true */
+    this.getRequestToken = function(cb) {
+        var requestTokenUrl = "/api/v1/oauth/withings";
+        $http.get(requestTokenUrl)
+            .success(function(data) {
+                cb(null, data);
+            })
+            .error(function(data) {
+                cb(data);
+            });
+    };
+    
+    this.getAccessToken = function(cb) {
+        var accessTokenUrl = "/api/v1/oauth/withings/oauth_callback";
+        $http.get(accessTokenUrl)
+            .success(function(data) {
+                cb(null, data);
+            })
+            .error(function(data) {
+                cb(data);
+            });
+    }
 }
