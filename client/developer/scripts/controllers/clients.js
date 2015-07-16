740f5171280d777b1a1d1f11845ca3a3733095b5
Ekavali Mishra <ekavali@amida-tech.com>
2015-07-16 14:49:30 -0400

    logout

--- client/developer/scripts/controllers/clients.js
+++ client/developer/scripts/controllers/clients.js
@@ -15,38 +15,17 @@ Clients.$inject = ['$location', 'authentication'];
 
 //function Clients($location, login, authentication) {
 function Clients($location, authentication) {
-    // /* jshint validthis: true */
-    // var vm = this;
-    // vm.ClientsLogin = ClientsLogin;
-
-    // activate();
-
-    // function activate() {
-
-    //     redirectUser();
-
-    //     function redirectUser() {
-    //         authentication.authStatus(function (err, res) {
-    //             if (err) {
-    //                 throw err;
-    //             } else {
-    //                 if (res) {
-    //                     $location.path('/Clients');
-    //                 }
-    //             }
-    //         });
-    //     }
-    // }
-
-    // function ClientsLogin() {
-    //     //    login.login(vm.inputLogin, vm.inputPassword, function (err) {
-    //     authentication.login(vm.inputLogin, vm.inputPassword, function (err) {
-    //         if (err) {
-    //             vm.error = err;
-    //         } else {
-    //             $location.path('/Clients');
-    //         }
-    //     });
-    // }
+    var vm = this;
+    vm.devLogout = function() {
+        authentication.logout(function (err) {
+            if (err) {
+                vm.error = err;
+            } else {
+                // $scope.loginStatus = false;
+                //$location.path('/');
+                $location.path('/developer');
+            }
+        });
+    }
 
 }
